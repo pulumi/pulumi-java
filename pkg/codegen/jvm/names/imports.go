84333a1f0508577efc0656b5ace2459b685bb2c2
Anton Tayanovskyy <anton@pulumi.com>
2022-02-04 18:06:04 -0500

    Remove comment

--- pkg/codegen/jvm/names/imports.go
+++ pkg/codegen/jvm/names/imports.go
@@ -101,28 +101,3 @@ func (i *Imports) Ref(name FQN) string {
 	}
 	return name.ToString()
 }
-
-/*
-
-   It it sounds like we should track the tree of scopes.
-
-   Because if we're changing imports,
-   then under the new imports, in all known scopes we need to make sure that
-
-     Ident resolves to the same as before.
-
-
-   Wait, what exactly is the problem?
-
-   adding an import
-
-   import x.y.z.Name;
-
-   makes Name resolve to x.y.z.Name;
-
-   So Name is no longer safe.
-
-   If we already used Name to refer to a.b.Name, then we can't add that import.
-
-
-*/
