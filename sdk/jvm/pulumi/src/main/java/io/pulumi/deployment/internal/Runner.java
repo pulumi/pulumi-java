6714154f603dad84e9b242adb2545487aea4010b
Paweł Prażak <pprazak@virtuslab.com>
2022-02-04 18:49:17 +0100

    Port SDK changes v3.5.1-v3.6.0
    - no Automation API

--- sdk/jvm/pulumi/src/main/java/io/pulumi/deployment/internal/Runner.java
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/deployment/internal/Runner.java
@@ -4,12 +4,15 @@ import io.pulumi.Stack;
 import io.pulumi.resources.StackOptions;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 public interface Runner {
+    List<Exception> getSwallowedExceptions();
+
     <T> void registerTask(String description, CompletableFuture<T> task);
 
     CompletableFuture<Integer> runAsyncFuture(Supplier<CompletableFuture<Map<String, Optional<Object>>>> callback, @Nullable StackOptions options);
@@ -18,5 +21,5 @@ public interface Runner {
 
     <T extends Stack> CompletableFuture<Integer> runAsync(Supplier<T> stackFactory);
 
-//    <T extends Stack> CompletableFuture<Integer> runAsync(ServiceProvider serviceProvider); // TODO ?!
+//    <T extends Stack> CompletableFuture<Integer> runAsync(ServiceProvider serviceProvider); // TODO: not sure how ServiceProvider translates to Java, is it needed?
 }
