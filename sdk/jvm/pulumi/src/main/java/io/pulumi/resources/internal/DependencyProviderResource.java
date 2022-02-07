cec7ffc42f1290bb022c11be1ff1049dba1fdd68
Paweł Prażak <pprazak@virtuslab.com>
2022-02-07 16:54:18 +0100

    Port SDK changes v3.8.0-v3.9.0
    - DependencyProviderResource improvements
    - fix DeploymentRunnerTest

--- sdk/jvm/pulumi/src/main/java/io/pulumi/resources/internal/DependencyProviderResource.java
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/resources/internal/DependencyProviderResource.java
@@ -2,6 +2,8 @@ package io.pulumi.resources.internal;
 
 import com.google.common.collect.ImmutableSet;
 import io.pulumi.core.OutputDefault;
+import io.pulumi.core.Tuples;
+import io.pulumi.core.Tuples.Tuple2;
 import io.pulumi.resources.ProviderResource;
 import io.pulumi.resources.Resource;
 import io.pulumi.resources.ResourceArgs;
@@ -13,8 +15,27 @@ import io.pulumi.resources.ResourceArgs;
  */
 public final class DependencyProviderResource extends ProviderResource {
     public DependencyProviderResource(String reference) {
-        super("", "", ResourceArgs.Empty, /* no options */ null, true);
+        super(parsePackage(reference), "", ResourceArgs.Empty, /* no options */ null, true);
 
+        var urnAndId = parseReference(reference);
+
+        ImmutableSet<Resource> resources = ImmutableSet.of(this);
+        this.setUrn(OutputDefault.of(resources, urnAndId.t1));
+        this.setId(OutputDefault.of(resources, urnAndId.t2));
+    }
+
+    private static String parsePackage(String reference) {
+        var urn = parseReference(reference).t1;
+        var urnParts = urn.split("::");
+        var qualifiedType = urnParts[2];
+        var qualifiedTypeParts = qualifiedType.split("\\$");
+        var type = qualifiedTypeParts[qualifiedTypeParts.length - 1];
+        var typeParts = type.split(":");
+        // type will be "pulumi:providers:<package>" and we want the last part.
+        return typeParts.length > 2 ? typeParts[2] : "";
+    }
+
+    private static Tuple2<String, String> parseReference(String reference) {
         var lastSep = reference.lastIndexOf("::");
         if (lastSep == -1) {
             throw new IllegalArgumentException(
@@ -22,9 +43,6 @@ public final class DependencyProviderResource extends ProviderResource {
         }
         var urn = reference.substring(0, lastSep);
         var id = reference.substring(lastSep + 2);
-
-        ImmutableSet<Resource> resources = ImmutableSet.of(this);
-        this.setUrn(OutputDefault.of(resources, urn));
-        this.setId(OutputDefault.of(resources, id));
+        return Tuples.of(urn, id);
     }
 }
