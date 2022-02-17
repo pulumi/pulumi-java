b2e97645dfe8d84c3f1d33391fb8c6891e0d2ba9
Paweł Prażak <pprazak@virtuslab.com>
2022-02-17 17:51:12 +0100

    Hide internal implementations of AssetOrArchive
    - use Internal in AssetOrArchive

--- sdk/jvm/pulumi/src/main/java/io/pulumi/core/AssetOrArchive.java
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/core/AssetOrArchive.java
@@ -1,5 +1,6 @@
 package io.pulumi.core;
 
+import io.pulumi.core.internal.Internal.Field;
 import io.pulumi.core.internal.annotations.InternalUse;
 
 import java.util.Objects;
@@ -8,9 +9,13 @@ import java.util.Objects;
  * Base class of @see {@link Asset}s and @see {@link Archive}s.
  */
 public abstract class AssetOrArchive {
+
     protected final String sigKey;
     protected final String propName;
     protected final Object value;
+
+    @Field
+    protected final Internal internal = new Internal();
 
     protected AssetOrArchive(String sigKey, String propName, Object value) {
         this.sigKey = Objects.requireNonNull(sigKey);
@@ -18,18 +23,25 @@ public abstract class AssetOrArchive {
         this.value = Objects.requireNonNull(value);
     }
 
-    @InternalUse
-    public String getSigKey() {
-        return sigKey;
-    }
+    public final class Internal {
 
-    @InternalUse
-    public Object getValue() {
-        return value;
-    }
+        private Internal() {
+            /* Empty */
+        }
 
-    @InternalUse
-    public String getPropName() {
-        return propName;
+        @InternalUse
+        public String getSigKey() {
+            return AssetOrArchive.this.sigKey;
+        }
+
+        @InternalUse
+        public Object getValue() {
+            return AssetOrArchive.this.value;
+        }
+
+        @InternalUse
+        public String getPropName() {
+            return AssetOrArchive.this.propName;
+        }
     }
 }
