b59f504477424f35d796c9b1ffb721b212aee775
Paweł Prażak <pprazak@virtuslab.com>
2022-02-18 17:45:27 +0100

    Less verbose test logs
    - mute logs from deliberate test errors

--- pkg/codegen/testing/test/testdata/mini-kubernetes/jvm-extras/src/test/java/io/pulumi/kubernetes/JsonAttrTests.java
+++ pkg/codegen/testing/test/testdata/mini-kubernetes/jvm-extras/src/test/java/io/pulumi/kubernetes/JsonAttrTests.java
@@ -2,6 +2,7 @@ package io.pulumi.kubernetes;
 
 import io.pulumi.Log;
 import io.pulumi.core.Input;
+import io.pulumi.core.internal.Internal;
 import io.pulumi.deployment.internal.EngineLogger;
 import io.pulumi.kubernetes.ProviderArgs;
 import io.pulumi.serialization.internal.Serializer;
@@ -16,7 +17,7 @@ class JsonAttrTests {
     void verifyKubeConfigNotDoubleEncoded() {
         var log = new Log(mock(EngineLogger.class));
         var providerArgs = ProviderArgs.builder().setKubeconfig(Input.of("kc")).build();
-        var map = providerArgs.internalToOptionalMapAsync(log).join();
+        var map = Internal.from(providerArgs).toOptionalMapAsync(log).join();
         var v = map.get("kubeconfig").get();
         var r = new Serializer(log).serializeAsync("", v, true).join();
         assertThat(r).isEqualTo("kc");
