f9f6d941692c9e0bf5568f4c5448279c4bff7745
Kyle Dixler <kyle@pulumi.com>
2022-03-02 12:33:22 -0800

    [codegen] built and added added providers/pulumi-docker/

--- providers/pulumi-docker/sdk/java/src/main/java/io/pulumi/docker/RegistryImage.java
+++ providers/pulumi-docker/sdk/java/src/main/java/io/pulumi/docker/RegistryImage.java
@@ -180,6 +180,22 @@ public class RegistryImage extends io.pulumi.resources.CustomResource {
         return this.sha256Digest;
     }
 
+    public interface BuilderApplicator {
+        public void apply(@Nullable RegistryImageArgs.Builder a);
+    }
+    private static io.pulumi.docker.RegistryImageArgs buildArgs(BuilderApplicator argsBuilder) {
+        final var builder = io.pulumi.docker.RegistryImageArgs.builder();
+        argsBuilder.apply(builder);
+        return builder.build();
+    }
+    /**
+     *
+     * @param name The _unique_ name of the resulting resource.
+     * @param argsBuilder A function that configures a passed builder.
+     */
+    public RegistryImage(String name, BuilderApplicator argsBuilder) {
+        this(name, buildArgs(argsBuilder), null);
+    }
     /**
      *
      * @param name The _unique_ name of the resulting resource.
