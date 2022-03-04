86355cc2ddce5aa752620f08209af7c9a10a4be6
Paweł Prażak <pprazak@virtuslab.com>
2022-03-04 21:03:42 +0100

    Drop 'internal' prefix from InputOutputData methods

--- sdk/jvm/pulumi/src/main/java/io/pulumi/core/internal/InputOutputData.java
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/core/internal/InputOutputData.java
@@ -233,8 +233,9 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
     }
 
     @InternalUse
-    public static <T> CompletableFuture<InputOutputData<List<T>>> internalAllHelperAsync(
-            List<CompletableFuture<InputOutputData<T>>> values) {
+    public static <T> CompletableFuture<InputOutputData<List<T>>> allHelperAsync(
+            List<CompletableFuture<InputOutputData<T>>> values
+    ) {
         return CompletableFutures.allOf(values)
                 .thenApply(dataList ->
                         builder(new ArrayList<T>(dataList.size()))
@@ -249,7 +250,9 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
     }
 
     @InternalUse
-    public static CompletableFuture<InputOutputData<Object>> internalCopyInputOutputData(@SuppressWarnings("rawtypes") @Nullable InputOutput obj) {
+    public static CompletableFuture<InputOutputData<Object>> copyInputOutputData(
+            @SuppressWarnings("rawtypes") @Nullable InputOutput obj
+    ) {
         if (obj == null) {
             return CompletableFuture.completedFuture(InputOutputData.empty());
         }
@@ -261,7 +264,7 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
             Input<T1> input1, Input<T2> input2, Input<T3> input3, Input<T4> input4,
             Input<T5> input5, Input<T6> input6, Input<T7> input7, Input<T8> input8
     ) {
-        return internalTupleHelperAsync(
+        return tupleHelperAsync(
                 (Internal.of(input1)).getDataAsync(),
                 (Internal.of(input2)).getDataAsync(),
                 (Internal.of(input3)).getDataAsync(),
@@ -277,7 +280,7 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
             Output<T1> output1, Output<T2> output2, Output<T3> output3, Output<T4> output4,
             Output<T5> output5, Output<T6> output6, Output<T7> output7, Output<T8> output8
     ) {
-        return internalTupleHelperAsync(
+        return tupleHelperAsync(
                 (Internal.of(output1)).getDataAsync(),
                 (Internal.of(output2)).getDataAsync(),
                 (Internal.of(output3)).getDataAsync(),
@@ -290,7 +293,7 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
     }
 
     @InternalUse
-    private static <T1, T2, T3, T4, T5, T6, T7, T8> CompletableFuture<InputOutputData<Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>>> internalTupleHelperAsync(
+    private static <T1, T2, T3, T4, T5, T6, T7, T8> CompletableFuture<InputOutputData<Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>>> tupleHelperAsync(
             CompletableFuture<InputOutputData<T1>> data1, CompletableFuture<InputOutputData<T2>> data2,
             CompletableFuture<InputOutputData<T3>> data3, CompletableFuture<InputOutputData<T4>> data4,
             CompletableFuture<InputOutputData<T5>> data5, CompletableFuture<InputOutputData<T6>> data6,
