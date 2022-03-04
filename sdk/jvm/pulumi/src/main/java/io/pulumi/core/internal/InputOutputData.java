d9261d633b7d21d62a6e73116c277db4f16ad78b
Paweł Prażak <pprazak@virtuslab.com>
2022-03-04 21:05:51 +0100

    Allow null in InputOutputData and its Builder

--- sdk/jvm/pulumi/src/main/java/io/pulumi/core/internal/InputOutputData.java
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/core/internal/InputOutputData.java
@@ -23,7 +23,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
- * Immutable internal type
+ * Immutable internal data type
  */
 @ParametersAreNonnullByDefault
 @InternalUse
@@ -39,21 +39,18 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
     private final boolean known;
     private final boolean secret;
 
+    /**
+     * See also: {@link #ofNullable(ImmutableSet, Object, boolean, boolean)},
+     * {@link #Empty}, {@link #Unknown}, {@link #EmptySecret}, {@link #UnknownSecret}
+     */
     @InternalUse
     private InputOutputData(ImmutableSet<Resource> resources, boolean isKnown, boolean isSecret) {
-        this.resources = Objects.requireNonNull(resources);
-        this.value = null;
-        this.known = isKnown;
-        this.secret = isSecret;
+        this(resources, null, isKnown, isSecret);
     }
 
-    private InputOutputData(ImmutableSet<Resource> resources, T value, boolean isKnown, boolean isSecret) {
+    private InputOutputData(ImmutableSet<Resource> resources, @Nullable T value, boolean isKnown, boolean isSecret) {
         this.resources = Objects.requireNonNull(resources);
-        if (isKnown) {
-            this.value = Objects.requireNonNull(value);
-        } else {
-            this.value = value;
-        }
+        this.value = value;
         this.known = isKnown; // can be true even with value == null (when empty)
         this.secret = isSecret;
     }
@@ -208,16 +205,14 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
             CompletableFuture<InputOutputData<T>> dataFuture,
             Function</* @Nullable */ T, CompletableFuture<InputOutputData<U>>> func
     ) {
-        return dataFuture.thenApply((InputOutputData<T> data) -> {
+        return dataFuture.thenCompose((InputOutputData<T> data) -> {
             ImmutableSet<Resource> resources = data.getResources();
 
-            // TODO: reference implementation had a special case for previews here
-            //       but is it really needed or can it be done differently?
             // During previews only, perform the apply only if the engine was able to
             // give us an actual value for this Output.
             if (!data.isKnown() && Deployment.getInstance().isDryRun()) {
                 return CompletableFuture.completedFuture(
-                        InputOutputData.ofNullable(resources, (U) null, false, data.isSecret())
+                        InputOutputData.ofNullable(resources, null, false, data.isSecret())
                 );
             }
 
@@ -229,7 +224,7 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
                     data.known && innerData.known,
                     data.secret || innerData.secret
             ));
-        }).thenCompose(Function.identity()); // TODO: this looks ugly, what am I doing wrong?
+        });
     }
 
     @InternalUse
@@ -313,39 +308,34 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
     }
 
     @InternalUse
-    public static <T> Builder<T> builder(T start) {
+    public static <T> Builder<T> builder(@Nullable T start) {
         return new Builder<>(start);
     }
 
     @InternalUse
     public static final class Builder<T> {
         private ImmutableSet.Builder<Resource> resources;
+        @Nullable
         private T value;
         private boolean isKnown;
         private boolean isSecret;
 
-        public Builder(T start) {
+        public Builder(@Nullable T start) {
             this(ImmutableSet.builder(), start, true, false);
         }
 
-        public Builder(ImmutableSet.Builder<Resource> resources, T value, boolean isKnown, boolean isSecret) {
+        public Builder(ImmutableSet.Builder<Resource> resources, @Nullable T value, boolean isKnown, boolean isSecret) {
             this.resources = Objects.requireNonNull(resources);
+            this.value = value;
             this.isKnown = isKnown;
-            if (isKnown) {
-                this.value = Objects.requireNonNull(value);
-            } else {
-                this.value = null;
-            }
             this.isSecret = isSecret;
         }
 
         @CanIgnoreReturnValue
         public <E> Builder<T> accumulate(InputOutputData<E> data, BiFunction<T, E, T> reduce) {
             this.resources = this.resources.addAll(data.resources);
+            this.value = reduce.apply(this.value, data.value);
             this.isKnown = this.isKnown && data.known;
-            if (this.isKnown) {
-                this.value = reduce.apply(this.value, data.value);
-            }
             this.isSecret = this.isSecret || data.secret;
             return this;
         }
@@ -357,17 +347,11 @@ public final class InputOutputData<T> implements Copyable<InputOutputData<T>> {
         }
 
         public <U, R> Builder<R> transform(InputOutputData<U> data, BiFunction<T, U, R> reduce) {
+            var resources = this.resources.addAll(data.resources);
+            var value = reduce.apply(this.value, data.value);
             var isKnown = this.isKnown && data.known;
-            R value = null;
-            if (isKnown) {
-                value = reduce.apply(this.value, data.value);
-            }
-            return new Builder<>(
-                    this.resources.addAll(data.resources),
-                    value,
-                    isKnown,
-                    this.isSecret || data.secret
-            );
+            var isSecret = this.isSecret || data.secret;
+            return new Builder<>(resources, value, isKnown, isSecret);
         }
 
         public <R> InputOutputData<R> build(Function<T, R> valuesBuilder) {
