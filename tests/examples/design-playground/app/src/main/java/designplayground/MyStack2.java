73931507922f7a6352b371f1d583d9011930cec0
Anton Tayanovskyy <anton@pulumi.com>
2022-03-09 17:52:57 -0500

    Tidy up

--- tests/examples/design-playground/app/src/main/java/designplayground/MyStack2.java
+++ tests/examples/design-playground/app/src/main/java/designplayground/MyStack2.java
@@ -6,17 +6,17 @@ import designplayground.alt1.ServiceSpecType;
 import io.pulumi.core.Output;
 
 import java.util.List;
-import designplayground.alt1.Service;
 
-// Should we use Builders? Probably yes, since we can then enforce required args.
-// Can we use a generic list builder or how we build lists?
-public class MyStack2  {
+public class MyStack2 {
 
     public MyStack2() {
         final var service = new Service("myservice", args ->
                 args.metadata($ -> $.clusterName("clusterName"))
                         .spec($ -> $.type(ServiceSpecType.LoadBalancer)
-                                .ports(List.of(ServicePortArgs.builder().port(80).targetPort("http").build())))
+                                .ports(List.of(ServicePortArgs.builder()
+                                        .port(80)
+                                        .targetPort("http")
+                                        .build())))
         );
 
         Output<String> apiVersion = service.getApiVersion();
