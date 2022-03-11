b586903c4dde27a7b5e6537663384c58cbe38e55
Paweł Prażak <pprazak@virtuslab.com>
2022-03-11 20:16:43 +0100

    Switch Input for Output in InputsTest, now OutputStaticsTest
    Part of #28

--- sdk/jvm/pulumi/src/test/java/io/pulumi/core/OutputStaticsTest.java
+++ sdk/jvm/pulumi/src/test/java/io/pulumi/core/OutputStaticsTest.java
@@ -9,11 +9,11 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 
-public class InputsTest {
+public class OutputStaticsTest {
 
     @Test
     void testListConcatNull() {
-        var result = Input.concatList(null, null);
+        var result = Output.concatList(null, null);
         var data = InputOutputTests.waitFor(result);
 
         assertThat(data.isEmpty()).isTrue();
@@ -24,7 +24,7 @@ public class InputsTest {
 
     @Test
     void testListConcatEmpty() {
-        var result = Input.concatList(Input.empty(), Input.empty());
+        var result = Output.concatList(Output.empty(), Output.empty());
         var data = InputOutputTests.waitFor(result);
 
         assertThat(data.isEmpty()).isTrue();
@@ -37,17 +37,17 @@ public class InputsTest {
     void testListConcatSimple() {
         final var outV2 = Output.of("V2");
         final var outV3 = Output.of("V3");
-        final var list1 = Input.<String>listBuilder()
+        final var list1 = Output.<String>listBuilder()
                 .add("V1")
                 .add(outV2)
                 .build();
 
-        final Input<List<String>> list2 = Input.<String>listBuilder()
+        final Output<List<String>> list2 = Output.<String>listBuilder()
                 .add(outV3)
                 .add("V4")
                 .build();
 
-        var result = Input.concatList(list1, list2);
+        var result = Output.concatList(list1, list2);
         var data = InputOutputTests.waitFor(result);
 
         assertThat(data.isEmpty()).isFalse();
@@ -63,11 +63,11 @@ public class InputsTest {
 
         // Check that the input maps haven't changed
         InputOutputTests.waitFor(
-                Output.tuple(list1, outV2.toInput()).applyVoid(t -> {
+                Output.tuple(list1, outV2).applyVoid(t -> {
                     assertThat(t.t1).hasSize(2);
                     assertThat(t.t1).contains("V1", t.t2);
                 }),
-                Output.tuple(list2, outV3.toInput()).applyVoid(t -> {
+                Output.tuple(list2, outV3).applyVoid(t -> {
                     assertThat(t.t1).hasSize(2);
                     assertThat(t.t1).containsOnly(t.t2, "V4");
                 })
@@ -76,7 +76,7 @@ public class InputsTest {
 
     @Test
     void testMapConcatNull() {
-        var result = Input.concatMap(null, null);
+        var result = Output.concatMap(null, null);
         var data = InputOutputTests.waitFor(result);
 
         assertThat(data.isEmpty()).isTrue();
@@ -87,7 +87,7 @@ public class InputsTest {
 
     @Test
     void testMapConcatEmpty() {
-        var result = Input.concatMap(Input.empty(), Input.empty());
+        var result = Output.concatMap(Output.empty(), Output.empty());
         var data = InputOutputTests.waitFor(result);
 
         assertThat(data.isEmpty()).isTrue();
@@ -101,18 +101,18 @@ public class InputsTest {
         final var outV2 = Output.of("V2");
         final var outV3 = Output.of("V3");
         final var outV3wrong = Output.of("V3_wrong");
-        final var map1 = Input.<String>mapBuilder()
+        final var map1 = Output.<String>mapBuilder()
                 .put("K1", "V1")
                 .put("K2", outV2)
                 .put("K3", outV3wrong)
                 .build();
 
-        final var map2 = Input.<String>mapBuilder()
+        final var map2 = Output.<String>mapBuilder()
                 .put("K3", outV3)
                 .put("K4", "V4")
                 .build();
 
-        var result = Input.concatMap(map1, map2);
+        var result = Output.concatMap(map1, map2);
         var data = InputOutputTests.waitFor(result);
 
         assertThat(data.isEmpty()).isFalse();
@@ -125,11 +125,11 @@ public class InputsTest {
 
         // Check that the input maps haven't changed
         InputOutputTests.waitFor(
-                Output.tuple(map1, outV3wrong.toInput()).applyVoid(t -> {
+                Output.tuple(map1, outV3wrong).applyVoid(t -> {
                     assertThat(t.t1).hasSize(3);
                     assertThat(t.t1).contains(entry("K3", t.t2));
                 }),
-                Output.tuple(map2, outV3.toInput()).applyVoid(t -> {
+                Output.tuple(map2, outV3).applyVoid(t -> {
                     assertThat(t.t1).hasSize(2);
                     assertThat(t.t1).contains(entry("K3", t.t2));
                 })
@@ -137,7 +137,7 @@ public class InputsTest {
     }
 
     @Test
-    void testInputMapEitherInitializer() {
+    void testOutputMapEitherInitializer() {
         var sample = SampleArgs.builder()
                 .add(Map.of(
                         "left", Either.ofLeft("testValue"),
@@ -152,7 +152,7 @@ public class InputsTest {
     }
 
     @Test
-    void testInputListEitherInitializer() {
+    void testOutputListEitherInitializer() {
         var sample = SampleArgs.builder()
                 .add(List.of(
                         Either.ofLeft("testValue"),
@@ -167,10 +167,10 @@ public class InputsTest {
 
     private static final class SampleArgs {
 
-        public final Input<List<Either<String, Integer>>> list;
-        public final Input<Map<String, Either<String, Integer>>> dict;
+        public final Output<List<Either<String, Integer>>> list;
+        public final Output<Map<String, Either<String, Integer>>> dict;
 
-        private SampleArgs(Input<List<Either<String, Integer>>> list, Input<Map<String, Either<String, Integer>>> dict) {
+        private SampleArgs(Output<List<Either<String, Integer>>> list, Output<Map<String, Either<String, Integer>>> dict) {
             this.list = list;
             this.dict = dict;
         }
@@ -180,8 +180,8 @@ public class InputsTest {
         }
 
         public static final class Builder {
-            private final Input.ListBuilder<Either<String, Integer>> list = Input.listBuilder();
-            private final Input.MapBuilder<Either<String, Integer>> dict = Input.mapBuilder();
+            private final Output.ListBuilder<Either<String, Integer>> list = Output.listBuilder();
+            private final Output.MapBuilder<Either<String, Integer>> dict = Output.mapBuilder();
 
             @CanIgnoreReturnValue
             public Builder add(List<Either<String, Integer>> list) {
@@ -202,27 +202,27 @@ public class InputsTest {
     }
 
     @Test
-    public void testNullableSecretifyInput() {
-        Input<String> res0_ = Input.ofNullable((String) null);
-        Input<String> res0 = res0_.asSecret();
+    public void testNullableSecretifyOutput() {
+        Output<String> res0_ = Output.ofNullable((String) null);
+        Output<String> res0 = res0_.asSecret();
         var data0 = InputOutputTests.waitFor(res0);
         assertThat(data0.getValueNullable()).isEqualTo(null);
         assertThat(data0.isSecret()).isTrue();
         assertThat(data0.isPresent()).isFalse();
         assertThat(data0.isKnown()).isTrue();
 
-        // stringify should not modify the original Input
+        // stringify should not modify the original Output
         var data0_ = InputOutputTests.waitFor(res0_);
         assertThat(data0_.isSecret()).isFalse();
 
-        Input<String> res1 = Input.ofNullable("test1").asSecret();
+        Output<String> res1 = Output.ofNullable("test1").asSecret();
         var data1 = InputOutputTests.waitFor(res1);
         assertThat(data1.getValueNullable()).isEqualTo("test1");
         assertThat(data1.isSecret()).isTrue();
         assertThat(data1.isPresent()).isTrue();
         assertThat(data1.isKnown()).isTrue();
 
-        Input<String> res2 = Input.ofNullable(Input.of("test2")).asSecret();
+        Output<String> res2 = Output.ofNullable(Output.of("test2")).asSecret();
         var data2 = InputOutputTests.waitFor(res2);
         assertThat(data2.getValueNullable()).isEqualTo("test2");
         assertThat(data2.isSecret()).isTrue();
