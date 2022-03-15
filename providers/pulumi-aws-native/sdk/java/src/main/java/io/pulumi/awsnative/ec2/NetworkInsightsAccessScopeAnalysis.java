5fc733768727004a8121f282205c55e1a1677082
Kyle Dixler <kyle@pulumi.com>
2022-03-15 12:32:42 -0700

    [codegen] built and added added providers/pulumi-aws-native/

--- providers/pulumi-aws-native/sdk/java/src/main/java/io/pulumi/awsnative/ec2/NetworkInsightsAccessScopeAnalysis.java
+++ providers/pulumi-aws-native/sdk/java/src/main/java/io/pulumi/awsnative/ec2/NetworkInsightsAccessScopeAnalysis.java
@@ -130,7 +130,7 @@ public class NetworkInsightsAccessScopeAnalysis extends io.pulumi.resources.Cust
 
     private static io.pulumi.resources.CustomResourceOptions makeResourceOptions(@Nullable io.pulumi.resources.CustomResourceOptions options, @Nullable Output<String> id) {
         var defaultOptions = io.pulumi.resources.CustomResourceOptions.builder()
-            .setVersion(Utilities.getVersion())
+            .version(Utilities.getVersion())
             .build();
         return io.pulumi.resources.CustomResourceOptions.merge(defaultOptions, options, id);
     }
--- providers/pulumi-aws-native/sdk/java/src/main/java/io/pulumi/awsnative/ec2/TransitGatewayMulticastDomainAssociation.java
+++ providers/pulumi-aws-native/sdk/java/src/main/java/io/pulumi/awsnative/ec2/TransitGatewayMulticastDomainAssociation.java
@@ -149,7 +149,7 @@ public class TransitGatewayMulticastDomainAssociation extends io.pulumi.resource
 
     private static io.pulumi.resources.CustomResourceOptions makeResourceOptions(@Nullable io.pulumi.resources.CustomResourceOptions options, @Nullable Output<String> id) {
         var defaultOptions = io.pulumi.resources.CustomResourceOptions.builder()
-            .setVersion(Utilities.getVersion())
+            .version(Utilities.getVersion())
             .build();
         return io.pulumi.resources.CustomResourceOptions.merge(defaultOptions, options, id);
     }
