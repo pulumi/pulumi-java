3eca2cb600ba365d3ec88470347f46aaa4b45eaa
Kyle Dixler <kyle@pulumi.com>
2022-03-16 10:53:04 -0700

    updated azure-java-static-website example

--- tests/examples/azure-java-static-website/app/src/main/java/staticwebsite/MyStack.java
+++ tests/examples/azure-java-static-website/app/src/main/java/staticwebsite/MyStack.java
@@ -3,14 +3,11 @@ package staticwebsite;
 import java.util.List;
 
 import io.pulumi.Stack;
-import io.pulumi.azurenative.cdn.Endpoint;
-import io.pulumi.azurenative.cdn.Profile;
+import io.pulumi.azurenative.cdn.*;
 import io.pulumi.azurenative.cdn.enums.QueryStringCachingBehavior;
 import io.pulumi.azurenative.cdn.inputs.DeepCreatedOriginArgs;
 import io.pulumi.azurenative.resources.ResourceGroup;
-import io.pulumi.azurenative.storage.Blob;
-import io.pulumi.azurenative.storage.StorageAccount;
-import io.pulumi.azurenative.storage.StorageAccountStaticWebsite;
+import io.pulumi.azurenative.storage.*;
 import io.pulumi.azurenative.storage.enums.Kind;
 import io.pulumi.azurenative.storage.enums.SkuName;
 import io.pulumi.azurenative.storage.inputs.SkuArgs;
@@ -31,31 +28,31 @@ public final class MyStack extends Stack {
         var resourceGroup = new ResourceGroup("resourceGroup");
 
         var storageAccount = new StorageAccount("storageaccount",
-                $ -> $.kind(Either.ofRight(Kind.StorageV2))
+                StorageAccountArgs.builder().kind(Either.ofRight(Kind.StorageV2))
                         .resourceGroupName(resourceGroup.getName())
                         .sku(SkuArgs.builder()
                                 .name(Either.ofRight(SkuName.Standard_LRS))
-                                .build()));
+                                .build()).build());
 
         var staticWebsite = new StorageAccountStaticWebsite("staticWebsite",
-                $ -> $.accountName(storageAccount.getName())
+                StorageAccountStaticWebsiteArgs.builder().accountName(storageAccount.getName())
                         .resourceGroupName(resourceGroup.getName())
                         .indexDocument("index.html")
-                        .error404Document("404.html"));
+                        .error404Document("404.html").build());
 
         var indexHtml = new Blob("index.html",
-                $ -> $.accountName(storageAccount.getName())
+                BlobArgs.builder().accountName(storageAccount.getName())
                         .resourceGroupName(resourceGroup.getName())
                         .containerName(staticWebsite.getContainerName())
                         .source(new Asset.FileAsset("./wwwroot/index.html"))
-                        .contentType("text/html"));
+                        .contentType("text/html").build());
 
         var notFoundHtml = new Blob("404.html",
-                $ -> $.accountName(storageAccount.getName())
+                BlobArgs.builder().accountName(storageAccount.getName())
                         .resourceGroupName(resourceGroup.getName())
                         .containerName(staticWebsite.getContainerName())
                         .source(new Asset.FileAsset("./wwwroot/404.html"))
-                        .contentType("text/html"));
+                        .contentType("text/html").build());
 
         // Web endpoint to the website.
         this.staticEndpoint = storageAccount.getPrimaryEndpoints()
@@ -63,17 +60,17 @@ public final class MyStack extends Stack {
 
         // (Optional) Add a CDN in front of the storage account.
         var profile = new Profile("profile",
-                $ -> $.resourceGroupName(resourceGroup.getName())
+                ProfileArgs.builder().resourceGroupName(resourceGroup.getName())
                         .location("global")
                         .sku(io.pulumi.azurenative.cdn.inputs.SkuArgs.builder()
                                 .name(Either.ofRight(io.pulumi.azurenative.cdn.enums.SkuName.Standard_Microsoft))
-                                .build()));
+                                .build()).build());
 
         var endpointOrigin = storageAccount.getPrimaryEndpoints()
                 .applyValue(pe -> pe.getWeb().replace("https://", "").replace("/", ""));
 
         var endpoint = new Endpoint("endpoint",
-                $ -> $.isHttpAllowed(false)
+                EndpointArgs.builder().isHttpAllowed(false)
                         .isHttpsAllowed(true)
                         .originHostHeader(endpointOrigin)
                         .origins(List.of(DeepCreatedOriginArgs.builder()
@@ -84,6 +81,7 @@ public final class MyStack extends Stack {
                         .profileName(profile.getName())
                         .queryStringCachingBehavior(QueryStringCachingBehavior.NotSet)
                         .resourceGroupName(resourceGroup.getName())
+                        .build()
                 );
 
 
