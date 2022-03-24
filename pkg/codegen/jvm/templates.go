ead64a21b21c0113ec93f632f9c39e3030ec648e
Paweł Prażak <pprazak@virtuslab.com>
2022-03-24 17:51:35 +0100

    Fix gradle-specific deps versions
    Part of #223

--- pkg/codegen/jvm/templates.go
+++ pkg/codegen/jvm/templates.go
@@ -148,7 +148,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.pulumi:pulumi:1.0.0+")
+    implementation("io.pulumi:pulumi:1.0.0")
     implementation("com.google.code.findbugs:jsr305:3.0.2")
     api("com.google.guava:guava:30.1-jre") // FIXME: do we really want to expose this dep?
     api("com.google.code.gson:gson:2.8.6") // make sure we don't clash with grpc deps
@@ -165,7 +165,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:${junitVersion}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
     testImplementation("org.assertj:assertj-core:3.20.2")
-    testImplementation("org.mockito:mockito-core:3.+")
+    testImplementation("org.mockito:mockito-core:3.12.4")
 }
 
 test {
