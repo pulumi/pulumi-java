60e2985f15e0614f4fa4185e3ca0f6acf551517e
Paweł Prażak <pprazak@virtuslab.com>
2022-03-30 23:02:37 +0200

    Add classpath inspection to SDK
    Part of #248

--- sdk/jvm/pulumi/src/main/java/io/pulumi/bootstrap/internal/Main.java
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/bootstrap/internal/Main.java
@@ -10,13 +10,18 @@ import java.util.Arrays;
 @InternalUse
 public class Main {
 
+    private static final String PACKAGES_CMD = "packages";
+
     public static void main(String[] args) {
-        var packagesCmd = Arrays.asList(args).contains("packages");
+        var packagesCmd = Arrays.asList(args).contains(PACKAGES_CMD);
         if (packagesCmd) {
             ImmutableMap<String, PulumiPackage> packages = PulumiPackages.fromClasspath(Main.class);
             var gson = new Gson();
-            System.out.println(gson.toJson(packages));
+            System.out.println(gson.toJson(packages.values()));
         } else {
+            System.err.printf("Unknown command '%s', available commands: %s\n",
+                    String.join(" ", args), String.join(", ", PACKAGES_CMD)
+            );
             System.exit(1);
         }
     }
