ece51bac2cebbe3bc29e6edb13e59db9488845b2
Anton Tayanovskyy <anton@pulumi.com>
2022-04-01 16:32:15 -0400

    Fix DependencyProviderResourceTests

--- sdk/jvm/pulumi/src/test/java/io/pulumi/resources/DependencyProviderResourceTests.java
+++ sdk/jvm/pulumi/src/test/java/io/pulumi/resources/DependencyProviderResourceTests.java
@@ -2,6 +2,7 @@ package io.pulumi.resources;
 
 import io.pulumi.core.OutputTests;
 import io.pulumi.core.internal.Internal;
+import io.pulumi.deployment.internal.CurrentDeployment;
 import io.pulumi.resources.internal.DependencyProviderResource;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,8 @@ public class DependencyProviderResourceTests {
     @Test
     void testGetPackage() {
         var ctx = OutputTests.testContext();
-        var res = new DependencyProviderResource("urn:pulumi:stack::project::pulumi:providers:aws::default_4_13_0");
+        var res = CurrentDeployment.withCurrentDeployment(ctx.deployment, () ->
+            new DependencyProviderResource("urn:pulumi:stack::project::pulumi:providers:aws::default_4_13_0"));
         assertThat(Internal.from(res).getPackage()).isEqualTo("aws");
     }
 }
