a9cab104151a567aea373e51669945faa6088492
Paweł Prażak <pprazak@virtuslab.com>
2022-04-04 20:48:17 +0200

    Introduce Pulumi.run

---
+++ sdk/jvm/pulumi/src/main/java/io/pulumi/Pulumi.java
@@ -0,0 +1,28 @@
+package io.pulumi;
+
+import io.pulumi.deployment.Deployment;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public interface Pulumi {
+
+    static Integer run(Supplier<Map<String, Optional<Object>>> callback) {
+        return runAsync(callback).join();
+    }
+
+    static CompletableFuture<Integer> runAsync(Supplier<Map<String, Optional<Object>>> callback) {
+        return Deployment.runAsync(callback);
+    }
+
+    static <S extends Stack> Integer runStack(Supplier<S> stackFactory) {
+        return runStackAsync(stackFactory).join();
+    }
+
+    static <S extends Stack> CompletableFuture<Integer> runStackAsync(Supplier<S> stackFactory) {
+        return Deployment.runAsyncStack(stackFactory);
+    }
+
+}
