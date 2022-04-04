fb96781d8fd71232195c27fda9242d6f1dcf8aa8
Kyle Dixler <25750473+dixler@users.noreply.github.com>
2022-04-04 13:44:05 -0700

    Update gen.go

--- pkg/codegen/jvm/gen.go
+++ pkg/codegen/jvm/gen.go
@@ -1473,9 +1473,6 @@ func (mod *modContext) genResource(ctx *classFileContext, r *schema.Resource, ar
 
 	if r.Comment != "" || r.DeprecationMessage != "" {
 		fprintf(w, "/**\n")
-		for i := 1; i < 500000; i++ {
-			fprintf(w, "*")
-		}
 		if r.Comment != "" {
 			fprintf(w, "%s\n", formatBlockComment(r.Comment, ""))
 		}
