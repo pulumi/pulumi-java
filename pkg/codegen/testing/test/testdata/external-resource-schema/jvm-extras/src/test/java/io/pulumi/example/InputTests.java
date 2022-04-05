092f1299d47a6d7abc3a5ba0967731b4094d0bc6
Paweł Prażak <pprazak@virtuslab.com>
2022-04-05 20:15:13 +0200

    Fix codegen tests

--- pkg/codegen/testing/test/testdata/external-resource-schema/jvm-extras/src/test/java/io/pulumi/example/InputTests.java
+++ pkg/codegen/testing/test/testdata/external-resource-schema/jvm-extras/src/test/java/io/pulumi/example/InputTests.java
@@ -32,35 +32,4 @@ class InputTests {
         assertThat(map).containsEntry("requiredNameArray", Optional.empty());
         assertThat(map).containsEntry("requiredNameMap", Optional.empty());
     }
-/* TODO: make this work
-    @Test
-    void testContainerArgs_simpleValues() {
-        var args = PetArgs.builder()
-                .setAge(1)
-                .setName()
-                .setNameArray()
-                .setNameMap()
-                .setRequiredName()
-                .setRequiredNameArray()
-                .setRequiredNameMap()
-                .build();
-
-        var map = args.internalToOptionalMapAsync().join();
-
-        assertThat(map).containsKey("age");
-        assertThat(map).containsKey("name");
-        assertThat(map).containsKey("nameArray");
-        assertThat(map).containsKey("nameMap");
-        assertThat(map).containsKey("requiredName");
-        assertThat(map).containsKey("requiredNameArray");
-        assertThat(map).containsKey("requiredNameMap");
-
-        assertThat(map).containsEntry("age", Optional.empty());
-        assertThat(map).containsEntry("name", Optional.empty());
-        assertThat(map).containsEntry("nameArray", Optional.empty());
-        assertThat(map).containsEntry("nameMap", Optional.empty());
-        assertThat(map).containsEntry("requiredName", Optional.empty());
-        assertThat(map).containsEntry("requiredNameArray", Optional.empty());
-        assertThat(map).containsEntry("requiredNameMap", Optional.empty());
-    }*/
 }
