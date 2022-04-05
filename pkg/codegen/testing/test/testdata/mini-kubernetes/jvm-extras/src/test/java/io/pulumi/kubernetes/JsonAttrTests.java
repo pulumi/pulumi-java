7f541c1682c7a31db74b725e09eb70c6d03ddf76
Paweł Prażak <pprazak@virtuslab.com>
2022-04-05 20:15:13 +0200

    Fix codegen tests

--- pkg/codegen/testing/test/testdata/mini-kubernetes/jvm-extras/src/test/java/io/pulumi/kubernetes/JsonAttrTests.java
+++ pkg/codegen/testing/test/testdata/mini-kubernetes/jvm-extras/src/test/java/io/pulumi/kubernetes/JsonAttrTests.java
@@ -2,6 +2,7 @@ package io.pulumi.kubernetes;
 
 import io.pulumi.Log;
 import io.pulumi.core.Output;
+import io.pulumi.core.internal.OutputData;
 import io.pulumi.core.internal.Internal;
 import io.pulumi.deployment.internal.EngineLogger;
 import io.pulumi.kubernetes.ProviderArgs;
@@ -13,12 +14,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 
 class JsonAttrTests {
+
+    static <T> OutputData<T> waitFor(Output<T> io) {
+        return Internal.of(io).getDataAsync().join();
+    }
+
     @Test
     void verifyKubeConfigNotDoubleEncoded() {
         var log = new Log(mock(EngineLogger.class));
         var providerArgs = ProviderArgs.builder().kubeconfig(Output.of("kc")).build();
-        var map = Internal.from(providerArgs).toOptionalMapAsync(log).join();
-        var v = map.get("kubeconfig").get();
+        var map = Internal.from(providerArgs).toMapAsync(log).join();
+        var v = waitFor(map.get("kubeconfig")).getValueNullable();
         var r = new Serializer(log).serializeAsync("", v, true).join();
         assertThat(r).isEqualTo("kc");
     }
