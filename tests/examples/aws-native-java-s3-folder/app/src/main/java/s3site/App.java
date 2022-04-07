c23c00fc8de40cc600d3e8290c154f75908371a8
Paweł Prażak <pprazak@virtuslab.com>
2022-04-07 18:05:15 +0200

    Fix examples after API changes

--- tests/examples/aws-native-java-s3-folder/app/src/main/java/s3site/App.java
+++ tests/examples/aws-native-java-s3-folder/app/src/main/java/s3site/App.java
@@ -1,10 +1,68 @@
+// from https://github.com/pulumi/examples/blob/8cc8b1a4384c8b33f70ba65c701e19daecfa6399/aws-ts-s3-folder/index.ts
 package s3site;
 
 import io.pulumi.Pulumi;
+import io.pulumi.asset.FileAsset;
+import io.pulumi.aws.s3.BucketObject;
+import io.pulumi.aws.s3.BucketObjectArgs;
+import io.pulumi.aws.s3.BucketPolicy;
+import io.pulumi.aws.s3.BucketPolicyArgs;
+import io.pulumi.awsnative.s3.Bucket;
+import io.pulumi.awsnative.s3.BucketArgs;
+import io.pulumi.awsnative.s3.inputs.BucketWebsiteConfigurationArgs;
+import io.pulumi.context.ExportContext;
+import io.pulumi.context.StackContext;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class App {
     public static void main(String[] args) {
-        int exitCode = Pulumi.runStack(MyStack::new);
+        int exitCode = Pulumi.run(App::stack);
         System.exit(exitCode);
+    }
+
+    private static ExportContext stack(StackContext ctx) {
+
+        final var siteBucket = new Bucket("s3-website-bucket",
+                BucketArgs.builder().websiteConfiguration(BucketWebsiteConfigurationArgs.builder()
+                        .indexDocument("index.html")
+                        .build()).build());
+
+        final String siteDir = "www/";
+        try {
+            for (var path : Files.walk(Paths.get(siteDir)).filter(Files::isRegularFile).toList()) {
+                var contentType = Files.probeContentType(path);
+                new BucketObject(path.toString().replace(siteDir, ""),
+                        BucketObjectArgs.builder().bucket(siteBucket.getId())
+                                .source(new FileAsset(path.toAbsolutePath().toString()))
+                                .contentType(contentType).build()
+                );
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        final var bucketPolicy = new BucketPolicy("bucketPolicy",
+                BucketPolicyArgs.builder().bucket(siteBucket.getId())
+                        .policy(siteBucket.getArn()
+                                .applyValue(bucketArn -> """
+                                            {
+                                                "Version":"2012-10-17",
+                                                "Statement":[{
+                                                    "Effect":"Allow",
+                                                    "Principal":"*",
+                                                    "Action":["s3:GetObject"],
+                                                    "Resource":["%s/*"]
+                                                }]
+                                            }
+                                        """.formatted(bucketArn))
+                        ).build());
+
+        ctx.export("bucketName", siteBucket.getBucketName());
+        ctx.export("websiteUrl", siteBucket.getWebsiteURL());
+
+        return ctx.exports();
     }
 }
--- tests/examples/eks-minimal/app/src/main/java/io/pulumi/example/eksminimal/App.java
+++ tests/examples/eks-minimal/app/src/main/java/io/pulumi/example/eksminimal/App.java
@@ -1,10 +1,58 @@
 package io.pulumi.example.eksminimal;
 
 import io.pulumi.Pulumi;
+import io.pulumi.aws.ec2.GetSubnetIds;
+import io.pulumi.aws.ec2.GetVpc;
+import io.pulumi.aws.ec2.inputs.GetSubnetIdsArgs;
+import io.pulumi.aws.ec2.inputs.GetVpcArgs;
+import io.pulumi.aws.ec2.outputs.GetVpcResult;
+import io.pulumi.context.ExportContext;
+import io.pulumi.context.StackContext;
+import io.pulumi.core.Output;
+import io.pulumi.deployment.InvokeOptions;
+import io.pulumi.eks.Cluster;
+import io.pulumi.eks.ClusterArgs;
+
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
-        int exitCode = Pulumi.runStack(MyStack::new);
+        int exitCode = Pulumi.run(App::stack);
         System.exit(exitCode);
+    }
+
+    private static ExportContext stack(StackContext ctx) {
+        var vpcIdOutput = Output.of(
+                GetVpc.invokeAsync(
+                        GetVpcArgs.builder().default_(true).build(),
+                        InvokeOptions.Empty
+                ).thenApply(GetVpcResult::getId)
+        );
+        ctx.export("vpcIdOutput", vpcIdOutput);
+
+        var subnetIdsOutput = vpcIdOutput
+                .apply(vpcId -> Output.of(GetSubnetIds.invokeAsync(GetSubnetIdsArgs.builder()
+                                .vpcId(vpcId)
+                                .build(),
+                        InvokeOptions.Empty)))
+                .applyValue(getSubnetIdsResult ->
+                        getSubnetIdsResult.getIds()
+                                .stream()
+                                .sorted()
+                                .limit(2)
+                                .collect(Collectors.toList()));
+
+        ctx.export("subnetIdsOutput", subnetIdsOutput.applyValue(vs -> String.join(",", vs)));
+
+        var cluster = new Cluster("my-cluster", ClusterArgs.builder()
+                .vpcId(vpcIdOutput)
+                .subnetIds(subnetIdsOutput)
+                .instanceType("t2.micro")
+                .minSize(1)
+                .maxSize(2)
+                .build());
+
+        ctx.export("kubeconfig", cluster.getKubeconfig());
+        return ctx.exports();
     }
 }
