6504b81c07bed73c2642e9965e5ccb826b44e5c8
Anton Tayanovskyy <anton@pulumi.com>
2022-04-08 15:49:59 -0400

    Fix testNullableSecretifyOutput

--- sdk/jvm/pulumi/src/test/java/io/pulumi/core/OutputsTest.java
+++ sdk/jvm/pulumi/src/test/java/io/pulumi/core/OutputsTest.java
@@ -8,7 +8,7 @@ public class OutputsTest {
 
     @Test
     public void testNullableSecretifyOutput() {
-        Output<String> res0_ = Output.of((String) null);
+        Output<String> res0_ = Output.ofNullable((String) null);
         Output<String> res0 = res0_.asSecret();
         var data0 = OutputTests.waitFor(res0);
         assertThat(data0.getValueNullable()).isEqualTo(null);
