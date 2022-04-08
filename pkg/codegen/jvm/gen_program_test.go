7f37133ef385369053049e9825aefc806c4deb8b
Zaid Ajaj <zaid.naom@gmail.com>
2022-04-08 16:51:35 +0200

    Initial program gen implementation

---
+++ pkg/codegen/jvm/gen_program_test.go
@@ -0,0 +1,68 @@
+package jvm
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type PclTestFile struct {
+	FileName string
+	FilePath string
+}
+
+func pclTestFilePaths(dir string) []PclTestFile {
+	var paths []PclTestFile
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return paths
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".pp") {
+			fullPath, _ := filepath.Abs(filepath.Join(dir, file.Name()))
+			pclTestFile := PclTestFile{FileName: file.Name(), FilePath: fullPath}
+			paths = append(paths, pclTestFile)
+		}
+	}
+
+	return paths
+}
+
+func directoryExists(dir string) bool {
+	_, err := os.Stat(dir)
+	return !os.IsNotExist(err)
+}
+
+var testdataPath = filepath.Join("..", "testing", "test", "testdata")
+
+func TestGenerateProgram(t *testing.T) {
+	pclFiles := pclTestFilePaths(testdataPath)
+	assert.NotEmpty(t, pclFiles)
+	for _, pclFile := range pclFiles {
+		t.Logf("Compiling %s", pclFile)
+		pclFileContents, err := ioutil.ReadFile(pclFile.FilePath)
+		assert.Nilf(t, err, "Could not read contents of PCL file %s", pclFile)
+		compiledFile, diagnostics, err := CompilePclToJava(pclFileContents, testdataPath)
+		if diagnostics != nil {
+			for _, diagError := range diagnostics.Errs() {
+				t.Logf("    Diagnostics: %s", diagError.Error())
+			}
+		}
+		outputDirName := strings.ReplaceAll(pclFile.FileName, ".pp", "-pp")
+		outputDir := filepath.Join(testdataPath, outputDirName)
+		if !directoryExists(outputDir) {
+			err := os.Mkdir(outputDir, os.ModePerm)
+			assert.Nilf(t, err, "Could not create directory %s", outputDir)
+		}
+
+		outputFile := filepath.Join(outputDir, "MyStack.java")
+		err = ioutil.WriteFile(outputFile, compiledFile, 0644)
+		assert.Nilf(t, err, "Could not write compiled Java file %s", outputFile)
+		t.Logf("Written compiled file %s", outputFile)
+	}
+}
