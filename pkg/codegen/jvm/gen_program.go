94d6b03f7187ae8d100f2128650f206fe8db0129
Zaid Ajaj <zaid.naom@gmail.com>
2022-04-09 14:17:32 +0200

    Implement outputs, configs, invokes and smarter ranges on iterables

--- pkg/codegen/jvm/gen_program.go
+++ pkg/codegen/jvm/gen_program.go
@@ -78,8 +78,22 @@ func containsFunctionCall(functionName string, nodes []pcl.Node) bool {
 	return foundRangeCall
 }
 
+func hasIterableResources(nodes []pcl.Node) bool {
+	for _, node := range nodes {
+		switch node.(type) {
+		case *pcl.Resource:
+			resource := node.(*pcl.Resource)
+			if resource.Options != nil && resource.Options.Range != nil {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 func containsRangeExpr(nodes []pcl.Node) bool {
-	return containsFunctionCall("readDir", nodes)
+	return hasIterableResources(nodes) || containsFunctionCall("readDir", nodes)
 }
 
 func GenerateProgram(program *pcl.Program) (map[string][]byte, hcl.Diagnostics, error) {
@@ -129,18 +143,60 @@ func (g *generator) genImport(w io.Writer, qualifiedName string) {
 	g.Fprintf(w, "import %s;\n", qualifiedName)
 }
 
+func (g *generator) makeIndent(w io.Writer) {
+	g.Fgenf(w, "%s", g.Indent)
+}
+func (g *generator) newline(w io.Writer) {
+	g.Fgen(w, "\n")
+}
+
+func (g *generator) emitOutputFields(w io.Writer, nodes []pcl.Node) {
+	for _, node := range nodes {
+		if outputVariable, isOutput := node.(*pcl.OutputVariable); isOutput {
+			g.Indented(func() {
+				g.makeIndent(w)
+				g.Fgenf(w, "@Export(type = String.class)")
+				g.newline(w)
+
+				g.makeIndent(w)
+				g.Fgenf(w, "private final Output<String> %s;", outputVariable.Name())
+				g.newline(w)
+			})
+		}
+	}
+}
+
+// Checks whether any of the input nodes is a configuration variable
+func containConfigVariables(nodes []pcl.Node) bool {
+	for _, node := range nodes {
+		switch node.(type) {
+		case *pcl.ConfigVariable:
+			return true
+		}
+	}
+
+	return false
+}
+
 // genPreamble generates import statements, class definition and constructor.
 func (g *generator) genPreamble(w io.Writer, nodes []pcl.Node) {
 	g.genImport(w, "java.util.*")
 	g.genImport(w, "java.io.*")
+	g.genImport(w, "java.nio.*")
 	g.genImport(w, "io.pulumi.*")
-	g.Fprint(w, "\n")
-
+	g.newline(w)
+	g.newline(w)
 	// Emit Stack class signature
 	g.Fprint(w, "public class MyStack extends Stack\n")
-	g.Fprint(w, "{\n")
+	g.Fprint(w, "{")
+	g.newline(w)
+	g.emitOutputFields(w, nodes)
+	g.newline(w)
 	g.Fprint(w, "    public MyStack()\n")
 	g.Fprint(w, "    {\n")
+	if containConfigVariables(nodes) {
+		g.Fprint(w, "        final var config = Config.of();\n")
+	}
 }
 
 // genInitialize generates the declaration and the call to the async Initialize method, and also fills stack
@@ -155,9 +211,24 @@ func (g *generator) generateRangeClass(w io.Writer) {
 		g.Fgenf(w, "%spublic T value;\n", g.Indent)
 		g.Fgenf(w, "%spublic String key;\n", g.Indent)
 		g.Fgenf(w, "%spublic Range(String key, T value) { this.value = value; this.key = key; }\n", g.Indent)
-		g.Fgenf(w, "%spublic static <TValue> Range<TValue> Create(String key, TValue input) {\n", g.Indent)
+		g.Fgenf(w, "%spublic static <TValue> Range<TValue> of(String key, TValue input) {\n", g.Indent)
 		g.Indented(func() {
 			g.Fgenf(w, "%sreturn new Range<TValue>(key, input);\n", g.Indent)
+		})
+		g.Fgenf(w, "%s}\n", g.Indent)
+		g.Fgenf(w, "%spublic T getValue() { return this.value; }\n", g.Indent)
+		g.Fgenf(w, "%spublic String getKey() { return this.key; }\n", g.Indent)
+		g.Fgenf(w, "%spublic static <TValue> List<Range<TValue>> of(Iterable<TValue> items) {\n", g.Indent)
+		g.Indented(func() {
+			g.Fgenf(w, "%svar results = new ArrayList<Range<TValue>>();\n", g.Indent)
+			g.Fgenf(w, "%sInteger counter = 0;\n", g.Indent)
+			g.Fgenf(w, "%sfor(var item : items) {\n", g.Indent)
+			g.Indented(func() {
+				g.Fgenf(w, "%sresults.add(new Range<TValue>(counter.toString(), item));\n", g.Indent)
+				g.Fgenf(w, "%scounter = counter + 1;\n", g.Indent)
+			})
+			g.Fgenf(w, "%s}\n", g.Indent)
+			g.Fgenf(w, "%sreturn results;\n", g.Indent)
 		})
 		g.Fgenf(w, "%s}\n", g.Indent)
 	})
@@ -173,7 +244,7 @@ func (g *generator) generateReadDirMethod(w io.Writer) {
 		g.Indented(func() {
 			g.Fgenf(w, "%sif (file.isFile()) {\n", g.Indent)
 			g.Indented(func() {
-				g.Fgenf(w, "%sresults.add(Range.Create(file.getName(), file.getName()));\n", g.Indent)
+				g.Fgenf(w, "%sresults.add(Range.of(file.getName(), file.getName()));\n", g.Indent)
 			})
 			g.Fgenf(w, "%s}\n", g.Indent)
 		})
@@ -228,7 +299,7 @@ func makeResourceName(baseName string, suffix string) string {
 	if suffix == "" {
 		return fmt.Sprintf(`"%s"`, baseName)
 	}
-	return "String.format(\"%s-%d\"," + baseName + ", " + suffix + ")"
+	return fmt.Sprintf(`"%s-"`, baseName) + " + " + suffix
 }
 
 func (g *generator) genResource(w io.Writer, resource *pcl.Resource) {
@@ -271,16 +342,31 @@ func (g *generator) genResource(w io.Writer, resource *pcl.Resource) {
 			g.Fgenf(w, "%s\n}\n", g.Indent)
 
 		} else {
-			// usual foreach
-			g.Fgenf(w, "%sfor (var range : %.12o)\n", g.Indent, rangeExpr)
+			// for each-loop through the elements to creates a resource from each one
+			switch rangeExpr.(type) {
+			case *model.FunctionCallExpression:
+				funcCall := rangeExpr.(*model.FunctionCallExpression)
+				switch funcCall.Name {
+				case pcl.IntrinsicConvert:
+					// wrap into range collection
+					g.Fgenf(w, "%sfor (var range : Range.of(%.12o))\n", g.Indent, rangeExpr)
+				default:
+					// assume function call returns a Range<T>
+					g.Fgenf(w, "%sfor (var range : /* %s */ %.12o)\n", g.Indent, funcCall.Name, rangeExpr)
+				}
+			default:
+				// wrap into range collection
+				g.Fgenf(w, "%sfor (var range : Range.of(%.12o))\n", g.Indent, rangeExpr)
+			}
+
 			g.Fgenf(w, "%s{\n", g.Indent)
 			g.Indented(func() {
-				suffix := ""
+				suffix := "range.getKey()"
 				g.Fgenf(w, "%s", g.Indent)
 				instantiate(makeResourceName(resource.Name(), suffix))
 				g.Fgenf(w, ";\n")
 			})
-			g.Fgenf(w, "%s}\n", g.Indent)
+			g.Fgenf(w, "%s}\n\n", g.Indent)
 		}
 
 	} else {
@@ -293,15 +379,30 @@ func (g *generator) genResource(w io.Writer, resource *pcl.Resource) {
 }
 
 func (g *generator) genConfigVariable(w io.Writer, configVariable *pcl.ConfigVariable) {
-
+	g.makeIndent(w)
+	if configVariable.DefaultValue != nil {
+		g.Fgenf(w, "final var %s = config.get(\"%s\").orElse(%v);",
+			makeValidIdentifier(configVariable.Name()),
+			configVariable.Name(),
+			configVariable.DefaultValue)
+	} else {
+		g.Fgenf(w, "final var %s = config.get(\"%s\");",
+			makeValidIdentifier(configVariable.Name()),
+			configVariable.Name())
+	}
+	g.newline(w)
 }
 
 func (g *generator) genLocalVariable(w io.Writer, localVariable *pcl.LocalVariable) {
-
+	g.makeIndent(w)
+	g.Fgenf(w, "final var %s = %v;", localVariable.Name(), localVariable.Definition.Value)
+	g.newline(w)
 }
 
 func (g *generator) genOutputAssignment(w io.Writer, outputVariable *pcl.OutputVariable) {
-
+	g.makeIndent(w)
+	g.Fgenf(w, "this.%s = %v;", outputVariable.Name(), outputVariable.Value)
+	g.newline(w)
 }
 
 func (g *generator) genNode(w io.Writer, n pcl.Node) {
