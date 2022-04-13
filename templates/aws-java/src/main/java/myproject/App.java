34fc6819aa91002bc9f1de8aa9a9851f3f76198f
Anton Tayanovskyy <anton@pulumi.com>
2022-04-13 09:27:51 -0400

    Add templates (#374)
    * Empty Maven template
    
    * Fix gradle template
    
    * Add aws-java template
    
    * Add gcp-java template
    
    * Add azure-java templates
    
    * Kubernetes template
    
    * Add make targets for testing templates
    
    * Start a CI job to test templates
    
    * Setup go 1.17
    
    * Pre-build template test dependencies
    
    * Adapt to new-style Stack entry point
    
    * Fixup build pre-reqs
    
    * Fix typo
    
    * Template tests need more time sadly
    
    * Configure cloud credentials for template tests
    
    * Update azure template to new function syntax

--- templates/aws-java/src/main/java/myproject/App.java
+++ templates/aws-java/src/main/java/myproject/App.java
@@ -2,11 +2,13 @@ package myproject;
 
 import io.pulumi.Pulumi;
 import io.pulumi.core.Output;
+import io.pulumi.aws.s3.Bucket;
 
 public class App {
     public static void main(String[] args) {
         int exitCode = Pulumi.run(ctx -> {
-            ctx.export("exampleOutput", Output.of("example"));
+            var bucket = new Bucket("my-bucket");
+            ctx.export("bucketName", bucket.getId());
             return ctx.exports();
         });
         System.exit(exitCode);
