d88b10078b04e43559ef174803dcd7ab23b76ae1
Paweł Prażak <pprazak@virtuslab.com>
2022-04-13 16:47:04 +0200

    Add unit test for Output.format

--- sdk/jvm/pulumi/src/test/java/com/pulumi/core/OutputsTest.java
+++ sdk/jvm/pulumi/src/test/java/com/pulumi/core/OutputsTest.java
@@ -2,6 +2,8 @@ package com.pulumi.core;
 
 import org.junit.jupiter.api.Test;
 
+import static io.pulumi.core.OutputTests.waitFor;
+import static io.pulumi.core.OutputTests.waitForValue;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OutputsTest {
@@ -10,19 +12,27 @@ public class OutputsTest {
     public void testNullableSecretifyOutput() {
         Output<String> res0_ = Output.ofNullable((String) null);
         Output<String> res0 = res0_.asSecret();
-        var data0 = OutputTests.waitFor(res0);
+        var data0 = waitFor(res0);
         assertThat(data0.getValueNullable()).isEqualTo(null);
         assertThat(data0.isSecret()).isTrue();
         assertThat(data0.isKnown()).isTrue();
 
         // stringify should not modify the original Input
-        var data0_ = OutputTests.waitFor(res0_);
+        var data0_ = waitFor(res0_);
         assertThat(data0_.isSecret()).isFalse();
 
         Output<String> res1 = Output.of("test1").asSecret();
-        var data1 = OutputTests.waitFor(res1);
+        var data1 = waitFor(res1);
         assertThat(data1.getValueNullable()).isEqualTo("test1");
         assertThat(data1.isSecret()).isTrue();
         assertThat(data1.isKnown()).isTrue();
+    }
+
+    @Test
+    public void testFormat() {
+        var foo = Output.of("foo");
+        var bar = Output.of("bar");
+        var result = Output.format("%s-%s", foo, bar);
+        assertThat(waitForValue(result)).isEqualTo("foo-bar");
     }
 }
