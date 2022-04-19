6e2b5679e04bd5bb2fb70b5213fe5609b4a92205
Paweł Prażak <pprazak@virtuslab.com>
2022-04-19 20:22:52 +0200

    Add StackReference integration test
    Reproduces #416

---
+++ tests/integration/stack-reference/src/main/java/com/pulumi/example/Main.java
@@ -0,0 +1,30 @@
+package com.pulumi.example;
+
+import com.pulumi.Pulumi;
+import com.pulumi.core.Output;
+import com.pulumi.resources.StackReference;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int exitCode = Pulumi.run(ctx -> {
+            ctx.log().info("step0");
+            var isLocal = ctx.config().getBoolean("local").orElse(false);
+            final String slug;
+            if (isLocal) {
+                slug = ctx.stackName();
+            } else {
+                var org = ctx.config().require("org");
+                slug = String.format(
+                        "%s/%s/%s", org, ctx.projectName(), ctx.stackName()
+                );
+            }
+            new StackReference(slug);
+
+            ctx.export("val", Output.of(List.of("a", "b")));
+            return ctx.exports();
+        });
+        System.exit(exitCode);
+    }
+}
---
+++ tests/integration/stack-reference/step2/src/main/java/com/pulumi/example/Main.java
@@ -0,0 +1,40 @@
+package com.pulumi.example;
+
+import com.pulumi.Pulumi;
+import com.pulumi.core.Output;
+import com.pulumi.resources.StackReference;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int exitCode = Pulumi.run(ctx -> {
+            ctx.log().info("step2");
+            var isLocal = ctx.config().getBoolean("local").orElse(false);
+            final String slug;
+            if (isLocal) {
+                slug = ctx.stackName();
+            } else {
+                var org = ctx.config().require("org");
+                slug = String.format(
+                        "%s/%s/%s", org, ctx.projectName(), ctx.stackName()
+                );
+            }
+            var a = new StackReference(slug);
+
+            var gotExpectedError = false;
+            try {
+                 a.getValueAsync(Output.of("val2")).join();
+            } catch (Exception e) {
+                gotExpectedError = true;
+            }
+
+            if (!gotExpectedError) {
+                throw new RuntimeException("Expected to get error trying to read secret from stack reference.");
+            }
+            ctx.export("gotExpectedError", Output.of(gotExpectedError));
+            return ctx.exports();
+        });
+        System.exit(exitCode);
+    }
+}
