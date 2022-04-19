e0eac7c32c63717e37e26834632b33ae327ce2ab
Zaid Ajaj <zaid.naom@gmail.com>
2022-04-19 17:06:45 +0200

    Lowercase function names, use codegen.internal for the package name

--- sdk/jvm/pulumi/src/main/java/com/pulumi/codegen/internal/Serialization.java
+++ sdk/jvm/pulumi/src/main/java/com/pulumi/codegen/internal/Serialization.java
@@ -1,4 +1,4 @@
-package com.pulumi.codegen;
+package com.pulumi.codegen.internal;
 
 import com.google.gson.Gson;
 
@@ -12,31 +12,31 @@ public class Serialization {
      * Converts the input Map(String, Object) into a JSON string.
      * This function maps to the toJSON({...}) function from PCL.
      */
-    public static String SerializeJson(Map<String, Object> object) {
+    public static String serializeJson(Map<String, Object> object) {
         var gson = new Gson();
         return gson.toJson(object);
     }
 
     /**
      * Accepts an array of Entry(String, Object) values to build up a Map(String, Object).
-     * The entries are usually created using the JsonProperty(String key, Object value) function.
+     * The entries are usually created using the jsonProperty(String key, Object value) function.
      */
     @SafeVarargs
-    public static Map<String, Object> JsonObject(Map.Entry<String, Object> ...entries) {
+    public static Map<String, Object> jsonObject(Map.Entry<String, Object> ...entries) {
         return Map.ofEntries(entries);
     }
 
     /**
-     * Creates an Entry(String, Object) used to build up a Map(String, Object) using JsonObject(entries) function.
+     * Creates an Entry(String, Object) used to build up a Map(String, Object) using jsonObject(entries) function.
      */
-    public static Map.Entry<String, Object> JsonProperty(String name, Object value) {
+    public static Map.Entry<String, Object> jsonProperty(String name, Object value) {
         return Map.entry(name, value);
     }
 
     /**
      * Accepts an array of objects to be serialized as a JSON array.
      */
-    public static Object[] JsonArray(Object ...values) {
+    public static Object[] jsonArray(Object ...values) {
         return values;
     }
 }
