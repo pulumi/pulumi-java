983e9f1fff8e73f8ae1ec8de30121cd35cddd586
Anton Tayanovskyy <anton@pulumi.com>
2022-04-20 14:54:15 -0400

    Pass validators: include sources and javadoc

--- maven/central-publish-demo/pulumi-demo/src/main/java/com/pulumi/demo/Demo.java
+++ maven/central-publish-demo/pulumi-demo/src/main/java/com/pulumi/demo/Demo.java
@@ -2,6 +2,9 @@ package com.pulumi.demo;
 
 import pulumirpc.Plugin;
 
+/**
+ * Demo class.
+ */
 public final class Demo {
     public static String testPlugin(Plugin plugin) {
         return "TODO";
