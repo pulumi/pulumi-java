e33eb52c610e1c076af93f89e25b86346de3b190
Anton Tayanovskyy <anton@pulumi.com>
2022-04-20 14:54:34 -0400

    Use the non-SNAPSHOT publishing to test on the staging repo

--- maven/central-publish-demo/publish.sh
+++ maven/central-publish-demo/publish.sh
@@ -6,8 +6,11 @@ export SIGNING_KEY_ID="2EC87BB8"
 export SIGNING_PASSWORD=$(pass gpg/support@pulumi.com/passphrase)
 export SIGNING_KEY=$(cat private-key.asc)
 
-export PULUMI_JAVA_DEMO_SDK_VERSION=0.0.1-SNAPSHOT
-export OSSRH_REPO_URL=https://s01.oss.sonatype.org/content/repositories/snapshots/
+# export PULUMI_JAVA_DEMO_SDK_VERSION=0.0.1-SNAPSHOT
+# export OSSRH_REPO_URL=https://s01.oss.sonatype.org/content/repositories/snapshots/
+
+export PULUMI_JAVA_DEMO_SDK_VERSION=0.0.1
+export OSSRH_REPO_URL=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/
 export OSSRH_USERNAME=t0yv0
 export OSSRH_PASSWORD=$(pass issues.sonatype.org/t0yv0/anton@pulumi.com)
 
