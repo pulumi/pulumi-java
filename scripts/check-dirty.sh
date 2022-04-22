1529653cef8c7281c7a0100969c6d745a63dac29
Anton Tayanovskyy <anton@pulumi.com>
2022-04-22 12:00:26 -0400

    Fix the script

--- scripts/check-dirty.sh
+++ scripts/check-dirty.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 git status -s -- providers
 
-[[ -n $(git status -s -- providers) ]] && echo "providers/ generated code is out of date" && exit 1
+if [[ -n $(git status -s -- providers) ]]; then
+    echo "providers/ generated code is out of date"
+    exit 1
+fi
