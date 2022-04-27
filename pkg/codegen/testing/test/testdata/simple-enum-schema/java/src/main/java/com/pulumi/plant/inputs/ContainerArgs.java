db1e5f95d183585e15920de5cedf2b70ca549856
Anton Tayanovskyy <anton@pulumi.com>
2022-04-26 22:42:33 -0400

    Accept

--- pkg/codegen/testing/test/testdata/simple-enum-schema/java/src/main/java/com/pulumi/plant/inputs/ContainerArgs.java
+++ pkg/codegen/testing/test/testdata/simple-enum-schema/java/src/main/java/com/pulumi/plant/inputs/ContainerArgs.java
@@ -120,9 +120,7 @@ public final class ContainerArgs extends com.pulumi.resources.ResourceArgs {
         }
 
         public ContainerArgs build() {
-            $.brightness = Codegen.objectProp("brightness", ContainerBrightness.class).output()
-                .arg($.brightness)
-                .def(com.pulumi.plant.tree_v1.enums.ContainerBrightness.One).getNullable();
+            $.brightness = Codegen.objectProp("brightness", ContainerBrightness.class).output().arg($.brightness).def(ContainerBrightness.One).getNullable();
             $.size = Objects.requireNonNull($.size, "expected parameter 'size' to be non-null");
             return $;
         }
