62d84a138ee50cdcd1cbb5b73e36a626837e05f0
Anton Tayanovskyy <anton@pulumi.com>
2022-05-02 11:56:49 -0400

    Update random to 4.5.0 (#520)
    * Updated random to 4.5.0
    
    * Updated random generated code to 4.5.0

--- providers/pulumi-random/sdk/java/src/main/java/com/pulumi/random/RandomUuid.java
+++ providers/pulumi-random/sdk/java/src/main/java/com/pulumi/random/RandomUuid.java
@@ -35,14 +35,16 @@ import javax.annotation.Nullable;
 @ResourceType(type="random:index/randomUuid:RandomUuid")
 public class RandomUuid extends com.pulumi.resources.CustomResource {
     /**
-     * Arbitrary map of values that, when changed, will trigger recreation of resource. See the main provider documentation for more information.
+     * Arbitrary map of values that, when changed, will trigger recreation of resource. See [the main provider
+     * documentation](../index.html) for more information.
      * 
      */
     @Export(name="keepers", type=Map.class, parameters={String.class, Object.class})
     private Output</* @Nullable */ Map<String,Object>> keepers;
 
     /**
-     * @return Arbitrary map of values that, when changed, will trigger recreation of resource. See the main provider documentation for more information.
+     * @return Arbitrary map of values that, when changed, will trigger recreation of resource. See [the main provider
+     * documentation](../index.html) for more information.
      * 
      */
     public Output<Optional<Map<String,Object>>> keepers() {
