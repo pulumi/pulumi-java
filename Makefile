5d05159e073b7b0afbe2eb45e093d0c42c9ec04e
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2022-05-03 14:41:35 -0400

    Initial check-in of pulumi-java

---
+++ Makefile
@@ -0,0 +1,71 @@
+PROJECT_NAME := Pulumi SDK
+SUB_PROJECTS := sdk/jvm sdk/dotnet sdk/nodejs sdk/python sdk/go
+include build/common.mk
+
+
+PROJECT         := github.com/pulumi/pulumi/pkg/v3/cmd/pulumi
+PROJECT_PKGS    := $(shell cd ./pkg && go list ./... | grep -v /vendor/)
+TESTS_PKGS      := $(shell cd ./tests && go list ./... | grep -v tests/templates | grep -v /vendor/)
+VERSION         := $(shell pulumictl get version)
+
+TESTPARALLELISM := 10
+
+ensure::
+	$(call STEP_MESSAGE)
+	@echo "cd sdk && go mod download"; cd sdk && go mod download
+	@echo "cd pkg && go mod download"; cd pkg && go mod download
+	@echo "cd tests && go mod download"; cd tests && go mod download
+
+build-proto::
+	cd sdk/proto && ./generate.sh
+
+.PHONY: generate
+generate::
+	$(call STEP_MESSAGE)
+	echo "Generate static assets bundle for docs generator"
+	cd pkg && go generate ./codegen/docs/gen.go
+
+build:: generate
+	cd pkg && go install -ldflags "-X github.com/pulumi/pulumi/pkg/v3/version.Version=${VERSION}" ${PROJECT}
+
+build_debug:: generate
+	cd pkg && go install -gcflags="all=-N -l" -ldflags "-X github.com/pulumi/pulumi/pkg/v3/version.Version=${VERSION}" ${PROJECT}
+
+install:: generate
+	cd pkg && GOBIN=$(PULUMI_BIN) go install -ldflags "-X github.com/pulumi/pulumi/pkg/v3/version.Version=${VERSION}" ${PROJECT}
+
+install_all:: install
+
+dist:: build
+	cd pkg && go install -ldflags "-X github.com/pulumi/pulumi/pkg/v3/version.Version=${VERSION}" ${PROJECT}
+
+# NOTE: the brew target intentionally avoids the dependency on `build`, as it does not require the language SDKs.
+brew:: BREW_VERSION := $(shell scripts/get-version HEAD)
+brew::
+	cd pkg && go install -ldflags "-X github.com/pulumi/pulumi/pkg/v3/version.Version=${BREW_VERSION}" ${PROJECT}
+
+lint::
+	for DIR in "pkg" "sdk" "tests" ; do \
+		pushd $$DIR ; golangci-lint run -c ../.golangci.yml --timeout 5m ; popd ; \
+	done
+
+test_fast:: build
+	cd pkg && $(GO_TEST_FAST) ${PROJECT_PKGS}
+
+test_build:: $(SUB_PROJECTS:%=%_install)
+	cd tests/integration/construct_component/testcomponent && yarn install && yarn link @pulumi/pulumi && yarn run tsc
+	cd tests/integration/construct_component/testcomponent-go && go build -o pulumi-resource-testcomponent
+	cd tests/integration/construct_component_slow/testcomponent && yarn install && yarn link @pulumi/pulumi && yarn run tsc
+	cd tests/integration/construct_component_plain/testcomponent && yarn install && yarn link @pulumi/pulumi && yarn run tsc
+	cd tests/integration/construct_component_plain/testcomponent-go && go build -o pulumi-resource-testcomponent
+	cd tests/integration/component_provider_schema/testcomponent && yarn install && yarn link @pulumi/pulumi && yarn run tsc
+	cd tests/integration/component_provider_schema/testcomponent-go && go build -o pulumi-resource-testcomponent
+
+test_all:: build test_build $(SUB_PROJECTS:%=%_install)
+	cd pkg && $(GO_TEST) ${PROJECT_PKGS}
+	cd tests && $(GO_TEST) -p=1 ${TESTS_PKGS}
+
+.PHONY: test_containers
+test_containers:
+	$(call STEP_MESSAGE)
+	./scripts/test-containers.sh ${VERSION}
---
+++ sdk/jvm/cmd/pulumi-language-jvm/main.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+
+	pbempty "github.com/golang/protobuf/ptypes/empty"
+	"github.com/pkg/errors"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/rpcutil"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/version"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"google.golang.org/grpc"
+)
+
+// Launches the language host RPC endpoint, which in turn fires up an RPC server implementing the
+// LanguageRuntimeServer RPC endpoint.
+func main() {
+	var tracing string
+	var root string
+	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
+	flag.StringVar(&root, "root", "", "Project root path to use")
+
+	// You can use the below flag to request that the language host load a specific executor instead of probing the
+	// PATH.  This can be used during testing to override the default location.
+	var givenExecutor string
+	flag.StringVar(&givenExecutor, "use-executor", "",
+		"Use the given program as the executor instead of looking for one on PATH")
+
+	flag.Parse()
+	args := flag.Args()
+	logging.InitLogging(false, 0, false)
+	cmdutil.InitTracing("pulumi-language-jvm", "pulumi-language-jvm", tracing)
+	var jvmExec string
+	switch {
+	case givenExecutor != "":
+		logging.V(3).Infof("language host asked to use specific executor: `%s`", givenExecutor)
+		jvmExec = givenExecutor
+	default:
+		pathExec, err := exec.LookPath("gradle")
+		if err != nil {
+			err = errors.Wrap(err, "could not find `gradle` on the $PATH")
+			cmdutil.Exit(err)
+		}
+
+		logging.V(3).Infof("language host identified executor from path: `%s`", pathExec)
+		jvmExec = pathExec
+	}
+
+	// Optionally pluck out the engine so we can do logging, etc.
+	var engineAddress string
+	if len(args) > 0 {
+		engineAddress = args[0]
+	}
+
+	// Fire up a gRPC server, letting the kernel choose a free port.
+	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{
+		func(srv *grpc.Server) error {
+			host := newLanguageHost(jvmExec, engineAddress, tracing)
+			pulumirpc.RegisterLanguageRuntimeServer(srv, host)
+			return nil
+		},
+	}, nil)
+	if err != nil {
+		cmdutil.Exit(errors.Wrapf(err, "could not start language host RPC server"))
+	}
+
+	// Otherwise, print out the port so that the spawner knows how to reach us.
+	fmt.Printf("%d\n", port)
+
+	// And finally wait for the server to stop serving.
+	if err := <-done; err != nil {
+		cmdutil.Exit(errors.Wrapf(err, "language host RPC stopped serving"))
+	}
+}
+
+// jvmLanguageHost implements the LanguageRuntimeServer interface
+// for use as an API endpoint.
+type jvmLanguageHost struct {
+	exec          string
+	engineAddress string
+	tracing       string
+}
+
+func newLanguageHost(exec, engineAddress, tracing string) pulumirpc.LanguageRuntimeServer {
+
+	return &jvmLanguageHost{
+		exec:          exec,
+		engineAddress: engineAddress,
+		tracing:       tracing,
+	}
+}
+
+// GetRequiredPlugins computes the complete set of anticipated plugins required by a program.
+func (host *jvmLanguageHost) GetRequiredPlugins(
+	ctx context.Context,
+	req *pulumirpc.GetRequiredPluginsRequest) (*pulumirpc.GetRequiredPluginsResponse, error) {
+
+	logging.V(5).Infof("GetRequiredPlugins: %v", req.GetProgram())
+
+	// TODO
+	plugins := []*pulumirpc.PluginDependency{}
+
+	// TODO
+	return &pulumirpc.GetRequiredPluginsResponse{Plugins: plugins}, nil
+}
+
+// Run is an RPC endpoint for LanguageRuntimeServer::Run
+func (host *jvmLanguageHost) Run(ctx context.Context, req *pulumirpc.RunRequest) (*pulumirpc.RunResponse, error) {
+	config, err := host.constructConfig(req)
+	if err != nil {
+		err = errors.Wrap(err, "failed to serialize configuration")
+		return nil, err
+	}
+	configSecretKeys, err := host.constructConfigSecretKeys(req)
+	if err != nil {
+		err = errors.Wrap(err, "failed to serialize configuration secret keys")
+		return nil, err
+	}
+
+	executable := host.exec
+	args := []string{}
+
+	// Run from source.
+	args = append(args, "run", "--console=plain")
+
+	if logging.V(5) {
+		commandStr := strings.Join(args, " ")
+		logging.V(5).Infoln("Language host launching process: ", host.exec, commandStr)
+	}
+
+	// Now simply spawn a process to execute the requested program, wiring up stdout/stderr directly.
+	var errResult string
+	cmd := exec.Command(executable, args...) // nolint: gas // intentionally running dynamic program name.
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = host.constructEnv(req, config, configSecretKeys)
+	if err := cmd.Run(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// If the program ran, but exited with a non-zero error code.  This will happen often, since user
+			// errors will trigger this.  So, the error message should look as nice as possible.
+			if status, stok := exiterr.Sys().(syscall.WaitStatus); stok {
+				err = errors.Errorf("Program exited with non-zero exit code: %d", status.ExitStatus())
+			} else {
+				err = errors.Wrapf(exiterr, "Program exited unexpectedly")
+			}
+		} else {
+			// Otherwise, we didn't even get to run the program.  This ought to never happen unless there's
+			// a bug or system condition that prevented us from running the language exec.  Issue a scarier error.
+			err = errors.Wrapf(err, "Problem executing program (could not run language executor)")
+		}
+
+		errResult = err.Error()
+	}
+
+	return &pulumirpc.RunResponse{Error: errResult}, nil
+}
+
+// constructEnv constructs an environ for `pulumi-language-jvm`
+// by enumerating all of the optional and non-optional evn vars present
+// in a RunRequest.
+func (host *jvmLanguageHost) constructEnv(req *pulumirpc.RunRequest, config, configSecretKeys string) []string {
+	env := os.Environ()
+
+	maybeAppendEnv := func(k, v string) {
+		if v != "" {
+			env = append(env, strings.ToUpper("PULUMI_"+k)+"="+v)
+		}
+	}
+
+	maybeAppendEnv("monitor", req.GetMonitorAddress())
+	maybeAppendEnv("engine", host.engineAddress)
+	maybeAppendEnv("project", req.GetProject())
+	maybeAppendEnv("stack", req.GetStack())
+	maybeAppendEnv("pwd", req.GetPwd())
+	maybeAppendEnv("dry_run", fmt.Sprintf("%v", req.GetDryRun()))
+	maybeAppendEnv("query_mode", fmt.Sprint(req.GetQueryMode()))
+	maybeAppendEnv("parallel", fmt.Sprint(req.GetParallel()))
+	maybeAppendEnv("tracing", host.tracing)
+	maybeAppendEnv("config", config)
+	maybeAppendEnv("config_secret_keys", configSecretKeys)
+
+	return env
+}
+
+// constructConfig json-serializes the configuration data given as part of a RunRequest.
+func (host *jvmLanguageHost) constructConfig(req *pulumirpc.RunRequest) (string, error) {
+	configMap := req.GetConfig()
+	if configMap == nil {
+		return "", nil
+	}
+
+	configJSON, err := json.Marshal(configMap)
+	if err != nil {
+		return "", err
+	}
+
+	return string(configJSON), nil
+}
+
+// constructConfigSecretKeys JSON-serializes the list of keys that contain secret values given as part of
+// a RunRequest.
+func (host *jvmLanguageHost) constructConfigSecretKeys(req *pulumirpc.RunRequest) (string, error) {
+	configSecretKeys := req.GetConfigSecretKeys()
+	if configSecretKeys == nil {
+		return "[]", nil
+	}
+
+	configSecretKeysJSON, err := json.Marshal(configSecretKeys)
+	if err != nil {
+		return "", err
+	}
+
+	return string(configSecretKeysJSON), nil
+}
+
+func (host *jvmLanguageHost) GetPluginInfo(ctx context.Context, req *pbempty.Empty) (*pulumirpc.PluginInfo, error) {
+	return &pulumirpc.PluginInfo{
+		Version: version.Version,
+	}, nil
+}
