4d26bc48eef83c314b1a8acf99a088dfa5092885
Paweł Prażak <pprazak@virtuslab.com>
2022-05-04 12:43:44 +0200

    Add StackReferenceArgs.Builder
    Fixes #534

--- CHANGELOG_PENDING.md
+++ CHANGELOG_PENDING.md
@@ -1,3 +1,7 @@
 ### Improvements
 
+- Fix #534: StackReferenceArgs can now be instantiated using StackReferenceArgs.builder() and StackReferenceArgs.Builder, 
+  also StackReferenceArgs.getName() was deprecated in favour of StackReferenceArgs.name()
+
 ### Bug Fixes
+
--- sdk/java/pulumi/src/main/java/com/pulumi/resources/StackReferenceArgs.java
+++ sdk/java/pulumi/src/main/java/com/pulumi/resources/StackReferenceArgs.java
@@ -7,26 +7,79 @@ import javax.annotation.Nullable;
 import java.util.Optional;
 
 /**
- * The set of arguments for constructing a StackReference resource.
+ * The set of arguments for constructing a {@link StackReference} resource.
  */
-public class StackReferenceArgs extends ResourceArgs {
-
-    public static final StackReferenceArgs Empty = new StackReferenceArgs(null) {
-        // Empty
-    };
+public final class StackReferenceArgs extends ResourceArgs {
 
     /**
-     * The name of the stack to reference.
+     * An empty {@link StackReferenceArgs} instance.
      */
+    public static final StackReferenceArgs Empty = StackReferenceArgs.builder().build();
+
     @Import(name = "name", required = true)
     @Nullable
-    public final Output<String> name;
+    private final Output<String> name;
 
     public StackReferenceArgs(@Nullable Output<String> name) {
         this.name = name;
     }
 
+    /**
+     * The name of the stack to reference.
+     *
+     * @deprecated use {@link #name()}
+     */
+    @Deprecated
     public Optional<Output<String>> getName() {
+        return name();
+    }
+
+    /**
+     * The name of the stack to reference.
+     */
+    public Optional<Output<String>> name() {
         return Optional.ofNullable(name);
+    }
+
+    /**
+     * @return a {@link Builder} instance
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * The builder for {@link StackReferenceArgs}.
+     */
+    public static final class Builder {
+        @Nullable
+        private Output<String> name;
+
+        /**
+         * @param name the name of the stack to reference.
+         * @return the {@link Builder}
+         * @see StackReferenceArgs#name()
+         */
+        public Builder name(@Nullable Output<String> name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * @param name the name of the stack to reference.
+         * @return the {@link Builder}
+         * @see StackReferenceArgs#name()
+         */
+        public Builder name(String name) {
+            this.name = Output.of(name);
+            return this;
+        }
+
+        /**
+         * @return a {@link StackReferenceArgs} instance created from this {@link Builder}
+         */
+        public StackReferenceArgs build() {
+            return new StackReferenceArgs(this.name);
+        }
     }
 }
