eb5ee5859219398a5f30a11504e8336db417c886
Anton Tayanovskyy <anton@pulumi.com>
2022-05-06 15:55:03 -0400

    Workaround Javadoc emission issues (#570)

--- pkg/codegen/java/utilities.go
+++ pkg/codegen/java/utilities.go
@@ -15,6 +15,17 @@ func formatBlockComment(comment string, indent string) string {
 	prefix := fmt.Sprintf("%s * ", indent)
 	comment = strings.ReplaceAll(comment, "*/", "*{@literal /}")
 	comment = codegen.FilterExamples(comment, "java")
+
+	// TODO it seems that inputs sometimes have at least `@(`
+	// inside java code examples. This quick fix targets it
+	// directly to escape `@` for Javadoc. Some refactoring is
+	// needed around javadoc emission: would be better to work on
+	// AST as inside codegen.FilterExamples so that the escaping
+	// is complete and applies only to comments, while not
+	// touching intentional Javadoc directives emitted such as
+	// `@return`.
+	comment = strings.ReplaceAll(comment, "@(", "{@literal @}(")
+
 	comment = html.EscapeString(comment)
 	comment = strings.Join(strings.Split(comment, "\n"), "\n"+prefix)
 	return prefix + comment
