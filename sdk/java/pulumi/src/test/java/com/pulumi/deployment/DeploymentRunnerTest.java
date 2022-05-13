13b33e9621e96d72f39ebfca4b1164db8b87246e
Paweł Prażak <pprazak@virtuslab.com>
2022-05-13 20:43:43 +0200

    Port DeploymentRunnerTest to new framework

--- sdk/java/pulumi/src/test/java/com/pulumi/deployment/DeploymentRunnerTest.java
+++ sdk/java/pulumi/src/test/java/com/pulumi/deployment/DeploymentRunnerTest.java
@@ -1,23 +1,22 @@
 package com.pulumi.deployment;
 
 import com.pulumi.core.Output;
-import com.pulumi.core.annotations.Export;
+import com.pulumi.core.TypeShape;
 import com.pulumi.core.internal.Internal;
-import com.pulumi.deployment.internal.DeploymentTests;
 import com.pulumi.exceptions.RunException;
-import com.pulumi.resources.Stack;
+import com.pulumi.test.PulumiTest;
 import com.pulumi.test.internal.InMemoryLogger;
-import com.pulumi.test.mock.MonitorMocksTest;
+import com.pulumi.test.internal.PulumiTestInternal;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 import java.util.logging.Level;
 
-import static com.pulumi.deployment.internal.DeploymentTests.cleanupDeploymentMocks;
+import static com.pulumi.deployment.internal.DeploymentTests.defaultLogger;
+import static com.pulumi.deployment.internal.Runner.ProcessExitedAfterLoggingUserActionableMessage;
 import static com.pulumi.test.internal.assertj.PulumiConditions.containsString;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,62 +24,61 @@ public class DeploymentRunnerTest {
 
     @AfterEach
     public void cleanup() {
-        cleanupDeploymentMocks();
+        PulumiTest.cleanup();
     }
 
     @Test
     void testTerminatesEarlyOnException() {
-        var mock = DeploymentTests.DeploymentMockBuilder.builder()
-            .setMocks(new MonitorMocksTest.MyMocks())
-            .setSpyGlobalInstance();
+        var logger = defaultLogger();
+        logger.setLevel(Level.OFF);
+        var mock = PulumiTestInternal.withDefaults()
+                .standardLogger(logger)
+                .useRealRunner()
+                .build();
 
-        mock.standardLogger.setLevel(Level.OFF);
-        var result = mock.tryTestAsync(TerminatesEarlyOnExceptionStack::new).join();
-
-        assertThat(mock.runner.getSwallowedExceptions()).hasSize(2);
-
-        assertThat(result.exceptions).isNotNull();
-        assertThat(result.exceptions).isNotEmpty();
-        assertThat(result.exceptions).hasSize(1);
-        assertThat(result.exceptions.get(0)).isExactlyInstanceOf(RunException.class);
-        assertThat(result.exceptions.get(0)).hasMessageContaining("Deliberate test error");
-        assertThat(result.resources).isNotNull();
-        assertThat(result.resources).isNotEmpty();
-        assertThat(result.resources).hasSize(1);
-        var stack = (TerminatesEarlyOnExceptionStack) result.resources.get(0);
-        assertThat(Internal.of(stack.slowOutput).getDataAsync()).isNotCompleted();
-        assertThat(Internal.of(stack.slowOutput).getValueNullable()).isNotCompleted();
-    }
-
-    public static class TerminatesEarlyOnExceptionStack extends Stack {
-        @Export(type = Integer.class)
-        public final Output<Integer> slowOutput;
-
-        public TerminatesEarlyOnExceptionStack() {
+        var result = mock.runTestAsync(ctx -> {
             Output.of(CompletableFuture.failedFuture(new RunException("Deliberate test error")));
-            this.slowOutput = Output.of(new CompletableFuture<Integer>()
-                    .completeOnTimeout(1, 60, TimeUnit.SECONDS));
-        }
+            ctx.export("slowOutput", Output.of(
+                    new CompletableFuture<Integer>()
+                            .completeOnTimeout(1, 60, TimeUnit.SECONDS)
+            ));
+        }).join();
+
+        assertThat(result.errors()).isNotNull();
+        assertThat(result.errors()).isNotEmpty();
+        assertThat(result.errors()).hasSize(1);
+        assertThat(result.errors()).haveAtLeastOne(containsString("Deliberate test error"));
+
+        assertThat(result.exceptions()).hasSize(2);
+        assertThat(result.exceptions().get(0)).isExactlyInstanceOf(CompletionException.class);
+        assertThat(result.exceptions().get(1)).isExactlyInstanceOf(RunException.class);
+        assertThat(result.exceptions().get(1)).hasMessageContaining("Deliberate test error");
+
+        assertThat(result.resources()).isNotNull();
+        assertThat(result.resources()).isNotEmpty();
+        assertThat(result.resources()).hasSize(1);
+        var stack = result.stack();
+        assertThat(Internal.of(stack.output("slowOutput", TypeShape.of(Object.class))).getDataAsync()).isNotCompleted();
+        assertThat(Internal.of(stack.output("slowOutput", TypeShape.of(Object.class))).getValueNullable()).isNotCompleted();
+
+        assertThat(result.exitCode()).isEqualTo(ProcessExitedAfterLoggingUserActionableMessage);
     }
 
     @Test
     void testLogsTaskDescriptions() {
         // The test requires Level.FINEST
         var logger = InMemoryLogger.getLogger(Level.FINEST, "DeploymentRunnerTest#testLogsTaskDescriptions");
-
-        var mock = DeploymentTests.DeploymentMockBuilder.builder()
-            .setMocks(new MonitorMocksTest.MyMocks())
-            .setStandardLogger(logger)
-            .setSpyGlobalInstance();
+        var mock = PulumiTestInternal.withDefaults()
+                .standardLogger(logger)
+                .useRealRunner()
+                .build();
 
         for (var i = 0; i < 2; i++) {
             final var delay = 100L + i;
-            mock.runner.registerTask(String.format("task%d", i), new CompletableFuture<Void>().completeOnTimeout(null, delay, TimeUnit.MILLISECONDS));
+            mock.runner().registerTask(String.format("task%d", i), new CompletableFuture<Void>().completeOnTimeout(null, delay, TimeUnit.MILLISECONDS));
         }
-        Supplier<CompletableFuture<Map<String, Output<?>>>> supplier =
-                () -> CompletableFuture.completedFuture(Map.of());
-        var code = mock.runner.runAsyncFuture(supplier, null).join();
-        assertThat(code).isEqualTo(0);
+        var code = mock.runner().registerAndRunAsync(() -> null).join();
+        assertThat(code.exitCode()).isEqualTo(0);
 
         var messages = logger.getMessages();
         for (var i = 0; i < 2; i++) {
