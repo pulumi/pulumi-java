186a500ece77af5f3c572d900449f4f9ccd0fc04
Paweł Prażak <pprazak@virtuslab.com>
2022-05-13 21:17:27 +0200

    Port DeploymentTest, ResourceTest to new framework

--- sdk/java/pulumi/src/test/java/com/pulumi/deployment/DeploymentTest.java
+++ sdk/java/pulumi/src/test/java/com/pulumi/deployment/DeploymentTest.java
@@ -1,28 +1,16 @@
 package com.pulumi.deployment;
 
-import com.pulumi.Config;
 import com.pulumi.core.Output;
-import com.pulumi.deployment.internal.DeploymentInternal;
 import com.pulumi.test.PulumiTest;
 import com.pulumi.test.internal.PulumiTestInternal;
-import com.pulumi.test.mock.MonitorMocksTest;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 import java.util.logging.Level;
 
-import static com.pulumi.deployment.internal.DeploymentTests.DeploymentMock;
-import static com.pulumi.deployment.internal.DeploymentTests.DeploymentMockBuilder;
-import static com.pulumi.deployment.internal.DeploymentTests.cleanupDeploymentMocks;
 import static com.pulumi.deployment.internal.DeploymentTests.defaultLogger;
 import static com.pulumi.deployment.internal.Runner.ProcessExitedAfterLoggingUserActionableMessage;
 import static com.pulumi.deployment.internal.Runner.ProcessExitedSuccessfully;
@@ -74,10 +62,10 @@ public class DeploymentTest {
 
         final var result = new AtomicInteger(0);
         var cf = new CompletableFuture<Integer>();
-        var runTaskOne = mock.runner().runAsyncFuture(() -> {
+        var runTaskOne = mock.runner().registerAndRunAsync(() -> {
             //noinspection unused
             Output<Integer> orphaned = Output.of(cf).applyValue(result::getAndSet); // the orphaned output
-            return CompletableFuture.completedFuture(Map.of()); // empty outputs
+            return null;
         });
 
         var triggered = cf.complete(42);
--- sdk/java/pulumi/src/test/java/com/pulumi/resources/ResourceTest.java
+++ sdk/java/pulumi/src/test/java/com/pulumi/resources/ResourceTest.java
@@ -1,11 +1,13 @@
 package com.pulumi.resources;
 
 import com.google.common.collect.ImmutableMap;
+import com.pulumi.Context;
 import com.pulumi.core.OutputTests;
 import com.pulumi.core.internal.Internal;
-import com.pulumi.test.mock.MockCallArgs;
-import com.pulumi.deployment.internal.DeploymentTests;
+import com.pulumi.test.PulumiTest;
 import com.pulumi.test.TestOptions;
+import com.pulumi.test.internal.PulumiTestInternal;
+import com.pulumi.test.mock.MockCallArgs;
 import com.pulumi.test.mock.MonitorMocks;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,30 +18,29 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-import static com.pulumi.deployment.internal.DeploymentTests.cleanupDeploymentMocks;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResourceTest {
 
-    private static DeploymentTests.DeploymentMock mock;
+    private static PulumiTestInternal mock;
 
     @BeforeAll
     public static void mockSetup() {
-        mock = DeploymentTests.DeploymentMockBuilder.builder()
-                .setOptions(new TestOptions(false))
-                .setMocks(new MyMocks())
-                .setSpyGlobalInstance();
+        mock = PulumiTestInternal.withOptions(new TestOptions(false))
+                .mocks(new MyMocks())
+                .useRealRunner()
+                .build();
     }
 
     @AfterAll
     static void cleanup() {
-        cleanupDeploymentMocks();
+        PulumiTest.cleanup();
     }
 
     @Test
     void testProviderPropagation() {
-        var resources = mock.testAsync(MyStack::new).join();
-
+        var result = mock.runTestAsync(ResourceTest::myStack).join();
+        var resources = result.resources();
         var resource = resources.stream()
                 .filter(r -> r.getResourceName().equals("testResource"))
                 .findFirst()
@@ -47,27 +48,25 @@ public class ResourceTest {
         assertThat(resource).isNotNull();
 
         var urn = OutputTests.waitFor(resource.getUrn()).getValueNullable();
-        var provider = Internal.from(resource).getProvider(resource.getResourceType());
+        assertThat(urn).isNotBlank();
 
+        var provider = Internal.from(resource).getProvider(resource.getResourceType());
         assertThat(provider).isPresent();
         assertThat(provider.get().getResourceName()).isEqualTo("testProvider");
         assertThat(provider.get().getResourceType()).isEqualTo("pulumi:providers:test");
     }
 
-    public static class MyStack extends Stack {
-        public MyStack() {
-            var mod = "test";
-            var provider = new ProviderResource(
-                    mod, "testProvider", ResourceArgs.Empty, CustomResourceOptions.Empty
-            );
-
-            var resource = new CustomResource(
-                    mod + ":a/b:c", "testResource", ResourceArgs.Empty,
-                    CustomResourceOptions.builder()
-                            .provider(provider)
-                            .build()
-            );
-        }
+    static void myStack(Context ctx) {
+        var mod = "test";
+        var provider = new ProviderResource(
+                mod, "testProvider", ResourceArgs.Empty, CustomResourceOptions.Empty
+        );
+        new CustomResource(
+                mod + ":a/b:c", "testResource", ResourceArgs.Empty,
+                CustomResourceOptions.builder()
+                        .provider(provider)
+                        .build()
+        );
     }
 
     static class MyMocks implements MonitorMocks {
