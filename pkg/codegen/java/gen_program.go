7c3bdcf1c5047e10788cbebaaa4fbd9803b84702
Ian Wahbe <ian@wahbe.com>
2022-07-14 18:00:21 -0700

    Don't panic during program-gen

--- pkg/codegen/java/gen_program.go
+++ pkg/codegen/java/gen_program.go
@@ -419,6 +419,11 @@ func (g *generator) collectFunctionCallImports(functionCall *model.FunctionCallE
 			switch invokeArgumentsExpr.(type) {
 			case *model.ObjectConsExpression:
 				argumentsExpr := invokeArgumentsExpr.(*model.ObjectConsExpression)
+				if functionSchema.Inputs == nil {
+					g.warnf(functionCall.Args[1].SyntaxNode().Range().Ptr(),
+						"cannot determine invoke argument type: the schema for %q has not inputs",
+						funcName)
+				}
 				argumentExprType := functionSchema.Inputs.InputShape
 				for _, importDef := range collectObjectImports(argumentsExpr, argumentExprType) {
 					imports = append(imports, importDef)
@@ -934,4 +939,12 @@ func compilePclToJava(source []byte, schemaPath string) ([]byte, hcl.Diagnostics
 	}
 
 	return javaPrograms["MyStack.java"], diagnostics, nil
+}
+
+func (g *generator) warnf(location *hcl.Range, reason string, args ...interface{}) {
+	g.diagnostics = append(g.diagnostics, &hcl.Diagnostic{
+		Severity: hcl.DiagWarning,
+		Summary:  fmt.Sprintf(reason, args...),
+		Subject:  location,
+	})
 }
