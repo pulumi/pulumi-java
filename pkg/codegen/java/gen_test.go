3379b6763ced6caa069c7c02ae2da9fa0e2a56a2
Anton Tayanovskyy <anton@pulumi.com>
2022-09-12 16:12:55 -0400

    Precise TestGeneratePackage gradle dependencies

--- pkg/codegen/java/gen_test.go
+++ pkg/codegen/java/gen_test.go
@@ -10,46 +10,102 @@ import (
 	"github.com/pulumi/pulumi/pkg/v3/codegen/testing/test"
 )
 
-var javaSpecificTests = []*test.SDKTest{
-	{
-		Directory:   "mini-azurenative",
-		Description: "Regression tests extracted from trying to codegen azure-natuve",
-	},
-	{
-		Directory:   "mini-awsnative",
-		Description: "Regression tests extracted from trying to codegen aws-native",
-	},
-	{
-		Directory:   "mini-awsclassic",
-		Description: "Regression tests extracted from trying to codegen aws",
-	},
-	{
-		Directory:   "mini-kubernetes",
-		Description: "Regression tests extracted from trying to codegen kubernetes",
-	},
-	{
-		Directory:   "mini-azuread",
-		Description: "Regression tests extracted from trying to codegen azuread",
-	},
-	{
-		Directory:   "mini-awsx",
-		Description: "Regression tests extracted from trying to codegen awsx",
-	},
-	{
-		Directory:   "output-funcs-edgeorder",
-		Description: "Testing EdgeOrder functions which return Output<T>",
-	},
-	{
-		Directory:   "jumbo-resources",
-		Description: "Testing resources with more than 255 properties",
-	},
+// Test case configuration for a GeneratePackage test.
+type generatePackageTestConfig struct {
+	packageInfo     PackageInfo
+	sdkTest         *test.SDKTest
+	hasRuntimeTests bool
 }
 
-func adaptTest(t *test.SDKTest) *test.SDKTest {
+// Configures tests that are maintained in the pulumi-java repository
+// specifially for Java codegen testing.
+func javaSpecificTests() []generatePackageTestConfig {
+	return []generatePackageTestConfig{
+		newGeneratePackageTestConfig(&test.SDKTest{
+			Directory:   "mini-azurenative",
+			Description: "Regression tests extracted from trying to codegen azure-natuve",
+		}),
+		newGeneratePackageTestConfig(&test.SDKTest{
+			Directory:   "mini-awsnative",
+			Description: "Regression tests extracted from trying to codegen aws-native",
+		}),
+		newGeneratePackageTestConfigWithExtras(&test.SDKTest{
+			Directory:   "mini-awsclassic",
+			Description: "Regression tests extracted from trying to codegen aws",
+		}, &PackageInfo{
+			Dependencies: map[string]string{
+				"com.google.protobuf:protobuf-java":      "3.12.0",
+				"com.google.protobuf:protobuf-java-util": "3.12.0",
+				"org.assertj:assertj-core":               "3.20.2",
+				"org.junit.jupiter:junit-jupiter-api":    "5.7.2",
+			},
+		}),
+		newGeneratePackageTestConfigWithExtras(&test.SDKTest{
+			Directory:   "mini-kubernetes",
+			Description: "Regression tests extracted from trying to codegen kubernetes",
+		}, &PackageInfo{
+			Dependencies: map[string]string{
+				"com.google.guava:guava":              "30.1-jre",
+				"org.mockito:mockito-core":            "3.12.4",
+				"org.assertj:assertj-core":            "3.20.2",
+				"org.junit.jupiter:junit-jupiter-api": "5.7.2",
+			},
+		}),
+		newGeneratePackageTestConfig(&test.SDKTest{
+			Directory:   "mini-azuread",
+			Description: "Regression tests extracted from trying to codegen azuread",
+		}),
+		newGeneratePackageTestConfig(&test.SDKTest{
+			Directory:   "mini-awsx",
+			Description: "Regression tests extracted from trying to codegen awsx",
+		}),
+		newGeneratePackageTestConfig(&test.SDKTest{
+			Directory:   "output-funcs-edgeorder",
+			Description: "Testing EdgeOrder functions which return Output<T>",
+		}),
+		newGeneratePackageTestConfigWithExtras(&test.SDKTest{
+			Directory:   "jumbo-resources",
+			Description: "Testing resources with more than 255 properties",
+		}, &PackageInfo{
+			Dependencies: map[string]string{
+				"org.assertj:assertj-core":            "3.20.2",
+				"org.junit.jupiter:junit-jupiter-api": "5.7.2",
+				"org.mockito:mockito-core":            "3.12.4",
+			},
+		}),
+	}
+}
+
+// Configures how language-agnostic test cases from pulumi/pulumi
+// repository are adapted to test Java, which tests are skipped, etc..
+func adaptTest(t *test.SDKTest) generatePackageTestConfig {
+	hasExtras := false
+	pkgInfo := PackageInfo{}
 	switch t.Directory {
+	case "simple-plain-schema":
+		hasExtras = true
+		pkgInfo = PackageInfo{
+			Dependencies: map[string]string{
+				"com.google.guava:guava":              "30.1-jre",
+				"org.assertj:assertj-core":            "3.20.2",
+				"org.junit.jupiter:junit-jupiter-api": "5.7.2",
+				"org.mockito:mockito-core":            "3.12.4",
+			},
+		}
+	case "simple-enum-schema":
+		hasExtras = true
+		pkgInfo = PackageInfo{
+			Dependencies: map[string]string{
+				"com.google.guava:guava":              "30.1-jre",
+				"org.assertj:assertj-core":            "3.20.2",
+				"org.junit.jupiter:junit-jupiter-api": "5.7.2",
+				"org.mockito:mockito-core":            "3.12.4",
+			},
+		}
 	case "external-resource-schema":
 		// TODO[pulumi/pulumi-java#13]
 		t.Skip = codegen.NewStringSet("java/any")
+		hasExtras = true
 	case "plain-schema-gh6957":
 		t.Skip = codegen.NewStringSet("java/any") // TODO
 	case "simple-methods-schema":
@@ -89,12 +145,76 @@ func adaptTest(t *test.SDKTest) *test.SDKTest {
 	case "go-nested-collections":
 		t.Skip = codegen.NewStringSet("java/any") // go-only
 	}
-	return t
+
+	if hasExtras {
+		return newGeneratePackageTestConfigWithExtras(t, &pkgInfo)
+	}
+
+	return newGeneratePackageTestConfig(t)
 }
 
-func testCases() []*test.SDKTest {
-	var ts []*test.SDKTest
-	ts = append(ts, javaSpecificTests...)
+func TestGeneratePackage(t *testing.T) {
+	for _, testCase := range testCases() {
+		testCase := testCase
+		checks := map[string]test.CodegenCheck{
+			"java/compile": compileGeneratedPackage,
+		}
+		if testCase.hasRuntimeTests {
+			checks["java/test"] = testGeneratedPackage
+		}
+		test.TestSDKCodegen(t, &test.SDKCodegenOptions{
+			GenPackage: func(tool string,
+				pkg *schema.Package,
+				extraFiles map[string][]byte) (map[string][]byte, error) {
+				pkg.Language = map[string]interface{}{
+					"java": testCase.packageInfo,
+				}
+				return GeneratePackage(tool, pkg, extraFiles)
+			},
+			Language:  "java",
+			TestCases: []*test.SDKTest{testCase.sdkTest},
+			Checks:    checks,
+		})
+	}
+}
+
+// Minimal test config that verifies code generation and compilation.
+func newGeneratePackageTestConfig(test *test.SDKTest) generatePackageTestConfig {
+	packageInfo := PackageInfo{
+		BuildFiles: "gradle",
+		Dependencies: map[string]string{
+			"com.pulumi:pulumi": "0.0.1",
+		},
+	}.WithDefaultDependencies()
+	return generatePackageTestConfig{
+		sdkTest:     test,
+		packageInfo: packageInfo,
+	}
+}
+
+// For codegen tests that populate java-extras dir with JUnit tests to
+// run on top of the generated code.
+func newGeneratePackageTestConfigWithExtras(test *test.SDKTest, info *PackageInfo) generatePackageTestConfig {
+	var packageInfo PackageInfo
+	if info != nil {
+		packageInfo = *info
+	}
+	packageInfo = packageInfo.With(PackageInfo{
+		BuildFiles: "gradle",
+		Dependencies: map[string]string{
+			"com.pulumi:pulumi": "0.0.1",
+		},
+	}).WithDefaultDependencies()
+	return generatePackageTestConfig{
+		sdkTest:         test,
+		packageInfo:     packageInfo,
+		hasRuntimeTests: true,
+	}
+}
+
+func testCases() []generatePackageTestConfig {
+	var ts []generatePackageTestConfig
+	ts = append(ts, javaSpecificTests()...)
 	for _, t := range test.PulumiPulumiSDKTests {
 		ts = append(ts, adaptTest(t))
 	}
@@ -107,42 +227,4 @@ func compileGeneratedPackage(t *testing.T, pwd string) {
 
 func testGeneratedPackage(t *testing.T, pwd string) {
 	test.RunCommand(t, "gradle build", pwd, "gradle", "build")
-}
-
-func TestGeneratePackage(t *testing.T) {
-	test.TestSDKCodegen(t, &test.SDKCodegenOptions{
-		GenPackage: generatePackage,
-		Language:   "java",
-		TestCases:  testCases(),
-		Checks: map[string]test.CodegenCheck{
-			"java/compile": compileGeneratedPackage,
-			"java/test":    testGeneratedPackage,
-		},
-	})
-}
-
-func generatePackage(tool string, pkg *schema.Package, extraFiles map[string][]byte) (map[string][]byte, error) {
-	pkgInfo := PackageInfo{
-		BuildFiles: "gradle",
-		Dependencies: map[string]string{
-			"com.pulumi:pulumi": "0.0.1",
-			// There are some sprinkled unit tests that
-			// complement testing the generated code at
-			// runtime, and this is the union of all their
-			// dependencies. It would be more precise to
-			// move the dependencies into the schema for
-			// the individual projects that happen to need
-			// them.
-			"com.google.guava:guava":                 "30.1-jre",
-			"com.google.protobuf:protobuf-java":      "3.12.0",
-			"com.google.protobuf:protobuf-java-util": "3.12.0",
-			"org.assertj:assertj-core":               "3.20.2",
-			"org.junit.jupiter:junit-jupiter-api":    "5.7.2",
-			"org.mockito:mockito-core":               "3.12.4",
-		},
-	}
-	pkg.Language = map[string]interface{}{
-		"java": pkgInfo.WithDefaultDependencies(),
-	}
-	return GeneratePackage(tool, pkg, extraFiles)
 }
