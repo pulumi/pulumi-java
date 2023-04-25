22c29cceb1e1f1ba621c45312207b6f232714899
Abhinav Gupta <abhinav@pulumi.com>
2023-04-25 14:20:34 -0700

    make/borrow-schema: Run on macOS
    The borrow-schema script relies on `ln --relative`
    which is not a flag available on BSD ln used on macOS.
    
    ```
    % ln -h
    usage: ln [-s [-F] | -L | -P] [-f | -i] [-hnv] source_file [target_file]
           ln [-s [-F] | -L | -P] [-f | -i] [-hnv] source_file ... target_dir
           link source_file target_file
    ```
    
    This makes the script use GNU coreutils-installed ln on macOS.
    This version is installed under `gln` by Homebrew.

--- scripts/borrow-schema.sh
+++ scripts/borrow-schema.sh
@@ -2,11 +2,25 @@
 
 set -euo pipefail
 
-
 SRC="$1"
 DEST=${SRC#pulumi/}
 DEST_DIR=$(dirname "$DEST")
 
 mkdir -p "$DEST_DIR"
 rm -f "$DEST"
-ln --relative -s -t "$DEST_DIR" "$SRC"
+
+LN="ln"
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS uses BSD ln, which doesn't support --relative.
+    # Look for GNU ln, which is installed as gln by Homebrew,
+    # or fail if it's not found.
+    if command -v gln >/dev/null 2>&1; then
+        LN=$(command -v gln)
+    else
+        echo >&2 "error: gln not found. Please install coreutils:"
+        echo >&2 "  brew install coreutils"
+        exit 1
+    fi
+fi
+
+"$LN" --relative -s -t "$DEST_DIR" "$SRC"
