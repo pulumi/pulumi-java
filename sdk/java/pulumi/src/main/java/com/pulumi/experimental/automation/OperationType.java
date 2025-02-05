7bdf3b7699ec774a67ab09524628d51d4ac309c4
Justin Van Patten <jvp@justinvp.com>
2025-02-05 11:47:02 -0800

    Simplify `OperationType`
    There's no need for the explicit int values.

--- sdk/java/pulumi/src/main/java/com/pulumi/experimental/automation/OperationType.java
+++ sdk/java/pulumi/src/main/java/com/pulumi/experimental/automation/OperationType.java
@@ -3,30 +3,20 @@
 package com.pulumi.experimental.automation;
 
 public enum OperationType {
-    UNKNOWN(-1),
-    SAME(0),
-    CREATE(1),
-    UPDATE(2),
-    DELETE(3),
-    REPLACE(4),
-    CREATE_REPLACEMENT(5),
-    DELETE_REPLACED(6),
-    READ(7),
-    READ_REPLACEMENT(8),
-    REFRESH(9),
-    READ_DISCARD(10),
-    DISCARD_REPLACED(11),
-    REMOVE_PENDING_REPLACE(12),
-    IMPORT(13),
-    IMPORT_REPLACEMENT(14);
-
-    private final int value;
-
-    OperationType(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    UNKNOWN,
+    SAME,
+    CREATE,
+    UPDATE,
+    DELETE,
+    REPLACE,
+    CREATE_REPLACEMENT,
+    DELETE_REPLACED,
+    READ,
+    READ_REPLACEMENT,
+    REFRESH,
+    READ_DISCARD,
+    DISCARD_REPLACED,
+    REMOVE_PENDING_REPLACE,
+    IMPORT,
+    IMPORT_REPLACEMENT,
 }
