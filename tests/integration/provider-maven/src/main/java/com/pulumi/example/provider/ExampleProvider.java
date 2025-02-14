5e7d1498ae9bce99cccbafb09ea4969c451ce9de
Mikhail Shilkov <github@mikhail.io>
2025-02-14 16:42:51 +0100

    Update a component example to use schema inference

--- tests/integration/provider-maven/src/main/java/com/pulumi/example/provider/ExampleProvider.java
+++ tests/integration/provider-maven/src/main/java/com/pulumi/example/provider/ExampleProvider.java
@@ -6,8 +6,13 @@ import java.util.concurrent.CompletableFuture;
 import java.util.HashMap;
 import java.util.Set;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import com.pulumi.core.internal.Internal;
 import com.pulumi.core.Output;
+import com.pulumi.provider.internal.infer.ComponentAnalyzer;
+import com.pulumi.provider.internal.infer.Metadata;
 import com.pulumi.provider.internal.models.*;
 import com.pulumi.provider.internal.properties.PropertyValue;
 import com.pulumi.provider.internal.properties.PropertyValueSerializer;
@@ -16,16 +21,11 @@ import com.pulumi.provider.internal.Provider;
 public class ExampleProvider implements Provider {
     @Override
     public CompletableFuture<GetSchemaResponse> getSchema(GetSchemaRequest request) {
-        var url = getClass().getResource("schema.json");
-        if (url == null) {
-            throw new RuntimeException("Could not find schema.json in package");
-        }
-        try {
-            String schema = new String(Files.readAllBytes(Paths.get(url.toURI())));
-            return CompletableFuture.completedFuture(new GetSchemaResponse(schema));
-        } catch (Exception ex) {
-            throw new RuntimeException("Could not read schema.json in package");
-        }
+        var metadata = new Metadata("javap", "0.1.0", "Sample Provider for testing");
+        var schema = ComponentAnalyzer.generateSchema(metadata, HelloWorld.class);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return CompletableFuture.completedFuture(new GetSchemaResponse(gson.toJson(schema)));
     }
 
     @Override
