e64be5060b0f49baf78ac4f38b2e404be8f20ce3
Swaruph <jyothiswaruphkolli@gmail.com>
2025-06-29 10:55:48 +0530

    add: package-info.java in packages - Added new files package-info.java for package descriptions - Specified in descriptions if a package is not meant for external use

---
+++ sdk/java/pulumi/src/main/java/com/pulumi/core/internal/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains internal annotations and metadata utilities.
+ * <p>
+ * <b>Note:</b> This package is primarily for internal use and is not intended for direct use by end users.
+ */
+package com.pulumi.core.internal.annotations;
