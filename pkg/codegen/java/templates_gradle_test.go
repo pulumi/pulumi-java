1157a0ee7c29d09a23b497c35e84bc16397445db
Anton Tayanovskyy <anton@pulumi.com>
2025-07-11 13:22:29 -0500

    Lint

--- pkg/codegen/java/templates_gradle_test.go
+++ pkg/codegen/java/templates_gradle_test.go
@@ -110,7 +110,7 @@ var accept = cmdutil.IsTruthy(os.Getenv("PULUMI_ACCEPT"))
 
 func assertFileContentIs(t *testing.T, filePath, expectedContent string) {
 	if accept {
-		err := os.WriteFile(filePath, []byte(expectedContent), 0600)
+		err := os.WriteFile(filePath, []byte(expectedContent), 0o600)
 		require.NoError(t, err)
 	}
 	actualContent, err := os.ReadFile(filePath)
