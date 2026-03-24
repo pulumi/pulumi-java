0beada937b888e4718956834a25caac9ae67aeb4
Ian Wahbe <me@iwahbe.com>
2026-03-24 10:07:51 +0100

    Fix l2-resource-option-additional-secret-outputs (#2098)
    Add codegen support for the `additionalSecretOutputs` resource option.
    The PCL binder types these values as `model.StringType` (unlike
    `ignoreChanges` which uses `ResourcePropertyType`), so the existing
    `genQuotedList` helper double-quotes them. Generate the list items
    directly with `%v` instead.

---
+++ pkg/cmd/pulumi-language-java/testdata/projects/l2-resource-option-additional-secret-outputs/src/main/java/generated_program/App.java
@@ -0,0 +1,33 @@
+package generated_program;
+
+import com.pulumi.Context;
+import com.pulumi.Pulumi;
+import com.pulumi.core.Output;
+import com.pulumi.simple.Resource;
+import com.pulumi.simple.ResourceArgs;
+import com.pulumi.resources.CustomResourceOptions;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        Pulumi.run(App::stack);
+    }
+
+    public static void stack(Context ctx) {
+        var withSecret = new Resource("withSecret", ResourceArgs.builder()
+            .value(true)
+            .build(), CustomResourceOptions.builder()
+                .additionalSecretOutputs("value")
+                .build());
+
+        var withoutSecret = new Resource("withoutSecret", ResourceArgs.builder()
+            .value(true)
+            .build());
+
+    }
+}
